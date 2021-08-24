9515d492163f34d352c95ee7493d5e331f2826c1
wangbinyan <1159018192@qq.com>
2021-08-24 11:05:56 +0800

    引入登录页面

---
+++ src/pages/login/login.vue
@@ -0,0 +1,90 @@
+<template>
+	<loginEmpower 
+    v-if="isLogin"
+    :loginInit="loginInit"
+    @suceess="loginSuccess"
+    @fail="loginFail"></loginEmpower>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+		    isLogin: false,
+		    loginInit: {},
+		  };
+		},
+		onLoad() {
+			console.log("login页面加载进来了", )
+			let loginInit = {};
+			loginInit.imgUrl = "./imgs/Lark20210823-152715.png"; // 登录logo
+			loginInit.imgWidth = 330; // logo的宽度
+			loginInit.imgHeight = 180; // logo的高度
+			loginInit.appId = "wxe4bac8776b7bcbbd"; //唯一标识 
+			loginInit.ctx = "app-guomeijia|ver-v8.2.2|plt-wxApp|cmpid-"; //登录ctx
+			loginInit.sharerUserId = uni.getStorageSync("sharerUserId") || ""; //邀请人userId
+			loginInit.sharerUnionId = uni.getStorageSync("sharerUnionId") || '';
+			loginInit.platForm = "wx"; // 小程序平台 微信：wx
+			loginInit.env = "test"
+			this.loginInit = loginInit
+			this.isLogin = true
+			console.log(this.loginInit)
+			console.log(this.isLogin)
+		},
+		methods: {
+			// 登录请求成功回调
+			loginSuccess(e) {
+			  // 登录，注册，绑定成功都在此函数回调内处理返回参数为
+			  // data：{ SCN 登录凭证，userId 用户id，gomeNickName 国美用户昵称，gomeHeadImgUrl 国美用户头像，loginName 登录昵称，gradeNO 会员等级，gradeName 会员等级昵称}
+			  // type 类型  login为登录， register为注册，bindMobile为绑定手机号
+			  let { data, type }  = e.detail; 
+			 
+			  let userInfo = uni.getStorageSync("userInfo");
+			  uni.setStorageSync("scn", data.SCN);
+			  uni.setStorageSync("userId", data.userId);
+			  uni.setStorageSync("userInfo", userInfo);
+			  uni.setStorageSync('date', Date.now());
+			  uni.setStorageSync("memberInfo", {
+			    gradeNO: data.gradeNO || '',
+			    gradeName: data.gradeName || '',
+			    loginName: data.loginName || ''
+			  });
+						
+			  console.log('type',type) // 根据 type 类型，跳转路由
+			},
+			// 登录请求失败回调
+			loginFail(e) {
+				uni.showToast({
+					title:`登录请求失败`,
+				})
+			}
+		},
+		mounted() {
+			console.log("ddddd")
+		},
+	}
+</script>
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
