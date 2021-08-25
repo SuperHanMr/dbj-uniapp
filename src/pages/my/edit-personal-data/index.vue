be6780de7cb6d51dac90759ee08b4f6186940cfc
李书梦 <2326107425@qq.com>
2021-08-25 14:47:39 +0800

    我的相关页面

---
+++ src/pages/my/edit-personal-data/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="base-info-container">
+		<view class="main-body">
+			<uni-list class="uni-list">
+				<uni-list-item title="头像" link to="" @click="onClick($event,1)" >
+					<template slot="footer">	
+						<image class="slot-image" src="../../../static/c1.png" ></image>
+					</template>
+				</uni-list-item>
+				<uni-list-item title="昵称" rightText="小仙女儿" link to="" @click="onClick($event,1)" >
+				</uni-list-item>
+				<uni-list-item title="手机号" rightText="就不告诉你我的手机号"  ></uni-list-item>
+			</uni-list>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			onClick(){
+				console.log("打印数据，哈哈哈")
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.base-info-container{
+	width: 100%;
+	height: 100%;
+	/* background-color: #FFC0CB; */
+}
+.base-info-container .main-body{
+	padding-top: 40rpx;
+}
+.slot-image{
+	border-radius: 50%;
+	width: 60rpx;
+	height: 60rpx;
+	background-color: yellowgreen;
+}
+</style>
--- src/pages/my/index/index.vue
+++ src/pages/my/index/index.vue
@@ -1,20 +1,46 @@
 <template>
-	<view class="">
-		我的
-		<uni-badge text="1"></uni-badge>
-		<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
-		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-		<view :active="active">
-			<view v-for="item in list">{{item}}</view>
+	<view class="my-container">
+		<view class="my-header">
+			<view class="avatar-img">
+				<image src="../../../static/60x60.png" mode="center" />
+			</view>
+			<view class="user-name">
+				用户名称
+				<view class="edit-info" @click="handlerPersonalData">
+					编辑个人资料
+					<uni-icons type=""></uni-icons>
+				</view>
+			</view>
+			<view class="set-up" @click="handleSetUp">
+				设置
+			</view>
 		</view>
-		<view @click="handlerToShop">购物车</view>
+		<view class="my-order">
+			<view class="order-header">
+			我的订单
+			</view>
+		</view>
+		<view class="personal-center">
+			<view class="personal-header">
+			个人中心
+				
+			</view>
+			<view class="personal-body">
+				<view class="main-body">
+					<view class="item-style" v-for="(item,index) in list" :key="index" @click="handlePersonalItem(item)">
+						{{item.value}}
+					</view> 
+					
+				</view>
+			</view>
+		</view>
 	</view>
 	
 	
 </template>
 
 <script>
-	import {uniBadge} from '@dcloudio/uni-ui'
+	import {uniBadge,} from '@dcloudio/uni-ui'
 	export default {
 	    components: {uniBadge},
 		data(){
@@ -22,16 +48,31 @@
 				list: [
 					{
 						key: "1",
-						value: "全部",
+						value: "我的收藏",
+						url:'../personal-center/my-collection/index'
+						
 					},
 					{
 						key: "2",
-						value: "待付款",
+						value: "我的关注",	
+						url:'../personal-center/my-attention/index'
 					},
 					{
 						key: "3",
-						value: "进行中",
-					}
+						value: "购物车",
+						url:'../shopping-car/index'
+					},
+					{
+						key: "4",
+						value: "我的钱包",
+						url:'../personal-center/my-wallet/index'
+					},
+					{
+						key: "5",
+						value: "投诉与建议",
+						url:'../personal-center/complaints-and-suggestions/index'
+					},
+					
 				]
 			}
 		},
@@ -41,10 +82,75 @@
 				uni.navigateTo({
 					url:"../shopping-car/index"
 				})
+			},
+			handleSetUp(){
+				console.log("点击设置")
+				uni.navigateTo({
+					url:"../set-up/index"
+				})
+			},
+			handlerPersonalData(){
+				console.log('编辑个人资料')
+				uni.navigateTo({
+					url:"../edit-personal-data/index"
+				})
+			},
+			handlePersonalItem(item){
+				console.log(item.url,"key")
+				uni.navigateTo({
+					url:item.url
+				})
 			}
 		}
 	}
 </script>
 
-<style>
+<style  scoped>
+	.my-container{
+		display:flex;
+		flex-direction: column;
+	}
+	.my-container .my-header{
+		display: flex;
+		flex-direction: row;
+		/* justify-content: space-between; */
+		align-items: center;
+		width: 100%;
+		background-color: #C0C0C0;
+	}
+	.my-container .my-header .avatar-img image{
+		width: 60rpx;
+		height: 60rpx;
+	}
+	.my-container .my-order{
+		width: 100%;
+		height: 400rpx;
+		background-color: #C0C0C0;
+		margin-top:20rpx ;
+		display: flex;
+	}
+	.personal-center{
+		width: 100%;
+		margin-top:20rpx ;
+		background-color: #C0C0C0;
+	}
+		
+	.personal-center .personal-body{
+		width: 100%;
+		background-color: pink;
+	}
+	.personal-center .personal-body .main-body{
+		display: flex;
+		flex:1;
+		overflow:auto;
+		
+	}
+	.personal-center .personal-body .main-body .item-style{
+		width: 200rpx;
+		margin:20rpx;
+		padding: 12rpx;
+		background-color: #4CD964;
+	}
+		
 </style>
+
