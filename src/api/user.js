c4f89a60a8e5b47e01aca2a4a8f16026b7421b4e
wangbinyan <1159018192@qq.com>
2021-08-26 15:02:01 +0800

    feat: 登录

--- src/api/user.js
+++ src/api/user.js
@@ -6,6 +6,6 @@ export function getUserInfo(params){
 export function getShoppingCartProductInfo(params){
 	return request.get(`/product/app/products/shoppingCart/userIds/${params}`,{})
 }
-export function oauthGomeInfo (params) {
-	return request.post('/app/oauth/gome/info/' ,params)
+export function oauthGomeInfo (data) {
+	return request.post('/app/oauth/gome/info/', data)
 }
