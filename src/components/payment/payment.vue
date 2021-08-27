8dceab2103bc45ee46a6550e3f202145784b6cc4
wangbinyan <1159018192@qq.com>
2021-08-27 20:44:31 +0800

    upd: 修改文件名称

---
+++ src/components/payment/payment.vue
@@ -0,0 +1,105 @@
+<template>
+	<view class="wrap">
+		<view class="pieces-price-unit">
+			<text class="pieces">共{{pieces}}件，</text>
+			<text class="unit">总计：￥</text>
+			<text class="price">{{__countPrice[0]}}</text>
+			<text class="flot">.{{__countPrice[1]}}</text>
+		</view>
+		<button class="btn">去结算</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "Payment",
+		props: {
+			pieces: {
+				type: Number,
+				default: 2
+			},
+			countPrice: {
+				type: String,
+				default: "1000.00"
+			}
+		},
+		computed: {
+			__countPrice() {
+				return this.countPrice.split(".")
+			}
+		},
+		data() {
+			return {}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.wrap {
+		height: 88rpx;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.pieces-price-unit {
+		display: flex;
+		justify-content: space-between;
+		flex-direction: row;
+		align-items: flex-end;
+		margin-bottom: 16rpx;
+		.pieces {
+			letter-spacing: 0;
+			font-size: 28rpx;
+			font-family: PingFangSC, PingFangSC-Regular;
+			font-weight: 400;
+			text-align: left;
+			color: #999999;
+		}
+
+		.unit {
+			letter-spacing: 0;
+			font-size: 28rpx;
+			font-family: PingFangSC, PingFangSC-Medium;
+			font-weight: 500;
+			text-align: left;
+			color: #333333;
+		}
+
+		.price {
+			letter-spacing: 0;
+			font-size: 48rpx;
+			font-family: Unnamed, Unnamed-Regular;
+			font-weight: 400;
+			text-align: left;
+			color: #333333;
+			vertical-align: bottom;
+			line-height: 48rpx;
+		}
+
+		.flot {
+			letter-spacing: 0;
+			font-size: 30rpx;
+			font-family: Unnamed, Unnamed-Regular;
+			font-weight: 400;
+			text-align: left;
+			color: #333333;
+		}
+	}
+
+	.btn {
+		margin: 0;
+		box-sizing: border-box;
+		width: 248rpx;
+		height: 88rpx;
+		opacity: 1;
+		background: linear-gradient(135deg, #53d5cc, #4fc9c9);
+		border-radius: 12rpx;
+		font-size: 32rpx;
+		font-family: PingFangSC, PingFangSC-Regular;
+		font-weight: 400;
+		text-align: center;
+		color: #ffffff;
+		line-height: 88rpx;
+	}
+</style>
---
+++ src/components/service-card/service-card.vue
@@ -0,0 +1,163 @@
+<template>
+	<view class="wrap" :style="{marginBottom: setting.isLast ? 0 : '24rpx'}">
+		<view class="title">
+			<text>{{setting.title}}</text>
+			<button><text>更换</text>
+				<view class="sanjiao"></view>
+			</button>
+		</view>
+		<view class="content">
+			<image :src="setting.cover"></image>
+			<view>
+				<view class="subtitle">{{setting.subtitle}}</view>
+				<view class="desc-area">
+					<text>按平方米计价</text>
+					<text>x {{setting.area}}</text>
+				</view>
+				<view class="price">
+					<text>￥{{setting.price}}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "ServiceCard",
+		props: {
+			setting: {
+				required: true,
+				type: Object,
+				default: () => {}
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	button {
+		padding: 0;
+		margin: 0;
+	}
+
+	.wrap {
+		width: 100%;
+		box-sizing: border-box;
+		height: 300rpx;
+		opacity: 1;
+		background: #fff;
+		border-radius: 40rpx;
+		padding: 32rpx;
+	}
+
+	.title {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		text {
+			height: 44rpx;
+			font-size: 32rpx;
+			font-family: PingFangSC, PingFangSC-Medium;
+			font-weight: 500;
+			text-align: left;
+			color: #333;
+			line-height: 44rpx;
+		}
+
+		button {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 96rpx;
+			height: 44rpx;
+			background: #f5f6f6;
+			border-radius: 12rpx;
+			font-family: PingFangSC, PingFangSC-Regular;
+			font-weight: 400;
+			text-align: center;
+			color: #333333;
+			margin-bottom: 24rpx;
+
+			text {
+				font-size: 22rpx;
+				margin-right: 8rpx;
+			}
+
+			.sanjiao {
+				border-bottom: 13rpx solid transparent;
+				border-left: 16rpx solid #999;
+				border-top: 13rpx solid transparent;
+				width: 0;
+				height: 0;
+			}
+		}
+	}
+
+	.content {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-left: 48rpx;
+		padding: 16rpx 24rpx 16rpx;
+		background: #f5f6f6;
+		border-radius: 16rpx;
+
+		image {
+			width: 136rpx;
+			height: 136rpx;
+			opacity: 1;
+			border-radius: 12rpx;
+			margin-right: 16rpx;
+		}
+
+		.subtitle {
+			width: 446rpx;
+			height: 36rpx;
+			font-size: 26rpx;
+			font-family: PingFangSC, PingFangSC-Regular;
+			font-weight: 400;
+			text-align: left;
+			color: #333333;
+			line-height: 36rpx;
+			margin-bottom: 8rpx;
+		}
+
+		.desc-area {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 32rpx;
+			font-size: 22rpx;
+			font-family: PingFangSC, PingFangSC-Regular;
+			font-weight: 400;
+			text-align: left;
+			color: #999999;
+			line-height: 32rpx;
+			margin-bottom: 30rpx;
+		}
+
+		.price {
+			text-align: right;
+
+			text {}
+		}
+	}
+
+	// <view>
+	// 	<view class="subtitle">{{setting.subtitle}}</view>
+	// 	<view class="desc-area">
+	// 		<text>按平方米计价</text>
+	// 		<text>x {{setting.area}}</text>
+	// 	</view>
+	// 	<view class="price">
+	// 		<text>￥{{setting.price}}</text>
+	// 	</view>
+	// </view>
+</style>
