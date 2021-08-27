3e655917e2b249c3d1a1361fc6d02651c4a0026b
贺成龙 <mr_dragonn@foxmail.com>
2021-08-27 17:23:59 +0800

    亲友团列表页增加左滑删除

--- src/pages/decorate/friends/friends.vue
+++ src/pages/decorate/friends/friends.vue
@@ -1,184 +1,240 @@
 <template>
-	<view class="content">
-		<button type="default" @click="isEditing=!isEditing">{{isEditing?'完成':'编辑'}}</button>
-		<view v-for="(item,index) in list" :key="index" class="item" @click="onSelect(index)">
-			<view v-if="selectList.includes(index)">
-				已选中
-			</view>
-			<view v-else>
-				未选中
+  <view>
+    <button
+      type="default"
+      @click="isEditing=!isEditing"
+    >{{isEditing?'完成':'编辑'}}</button>
+    <uni-swipe-action>
+      <uni-swipe-action-item
+        v-for="(item,index) in list"
+        :key="index"
+        :right-options="options"
+        @click="bindClick"
+        @change="swipeChange($event, index)"
+      >
+        <view class="item">
+          <view v-if="selectList.includes(index)">
+            已选中
+          </view>
+          <view v-else>
+            未选中
 
-			</view>
-			<view >
+          </view>
+          <view>
+            {{item.name}}
+          </view>
+        </view>
+      </uni-swipe-action-item>
+    </uni-swipe-action>
+    <view
+      v-if="!isEditing"
+      class="add-friends"
+      @click="toAddFriends"
+    >
+      添加亲友
 
-				{{item.name}}
-			</view>
-		</view>
-		<view v-if="!isEditing" class="add-friends" @click="toAddFriends">
-			添加亲友
+    </view>
+    <view
+      v-else
+      class="edit-friends"
+    >
+      <view
+        class="btn"
+        @click="addAll"
+      >
+        全选
+      </view>
+      <view
+        class="btn"
+        @click="deleteFriends"
+      >
+        删除
 
-		</view>
-		<view v-else class="edit-friends">
-			<view  class="btn" @click="addAll">
-				全选
-			</view>
-			<view class="btn" @click="deleteFriends">
-				删除
-
-			</view>
-		</view>
-	</view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				selectList:[],
-				isEditing:false,
-				list: [{
-						name: '张三',
-						type: '亲儿子',
-						id:0
-					},{
-						name: '张三',
-						type: '亲儿子',
-						id:1
-					},
-					{
-						name: '李四',
-						type: '儿子',
-						id:2
-					},
-					{
-						name: '王五',
-						type: '儿子',
-						id:3
-					},
-					{
-						name: '赵六',
-						type: '儿子',
-						id:4
-					},
-					{
-						name: '张三',
-						type: '亲儿子',
-						id:5
-					},
-					{
-						name: '李四',
-						type: '儿子',
-						id:6
-					},
-					{
-						name: '王五',
-						type: '儿子',
-						id:7
-					},
-					{
-						name: '赵六',
-						type: '儿子',
-						id:8
-					},
-					{
-						name: '张三',
-						type: '亲儿子',
-						id:9
-					},
-					{
-						name: '李四',
-						type: '儿子',
-						id:10
-					},
-					{
-						name: '王五',
-						type: '儿子',
-						id:11
-					},
-				]
-
-			};
-		},
-		methods:{
-			toAddFriends(){
-			uni.navigateTo({
-				url:"../add-friends/add-friends"
-			})
-			},
-			addAll(){
-				console.log(this.selectList.length)
-				console.log(this.list.length)
-				if(this.selectList.length!=this.list.length){
-				this.selectList=[]
-					this.list.forEach(e=>{
-						this.selectList.push(e.id)
-
-					})
-				}else{
-
-						this.selectList=[]
-				}
-
-			},
-			deleteFriends(){
-
-			},
-			onSelect(index){
-				let selectIndex = this.selectList.indexOf(index)
-				if(selectIndex>-1){
-					this.selectList.splice(selectIndex,1)
-
-				}else{
-					this.selectList.push(index)
-				}
-
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      options: [
+        {
+          text: "删除",
+          style: {
+            backgroundColor: "#dd524d",
+          },
+        },
+      ],
+      selectList: [],
+      isEditing: false,
+      list: [
+        {
+          name: "张三",
+          type: "亲儿子",
+          id: 0,
+        },
+        {
+          name: "张三",
+          type: "亲儿子",
+          id: 1,
+        },
+        {
+          name: "李四",
+          type: "儿子",
+          id: 2,
+        },
+        {
+          name: "王五",
+          type: "儿子",
+          id: 3,
+        },
+        {
+          name: "赵六",
+          type: "儿子",
+          id: 4,
+        },
+        {
+          name: "张三",
+          type: "亲儿子",
+          id: 5,
+        },
+        {
+          name: "李四",
+          type: "儿子",
+          id: 6,
+        },
+        {
+          name: "王五",
+          type: "儿子",
+          id: 7,
+        },
+        {
+          name: "赵六",
+          type: "儿子",
+          id: 8,
+        },
+        {
+          name: "张三",
+          type: "亲儿子",
+          id: 9,
+        },
+        {
+          name: "李四",
+          type: "儿子",
+          id: 10,
+        },
+        {
+          name: "王五",
+          type: "儿子",
+          id: 11,
+        },
+        {
+          name: "王五",
+          type: "儿子",
+          id: 7,
+        },
+        {
+          name: "赵六",
+          type: "儿子",
+          id: 8,
+        },
+        {
+          name: "张三",
+          type: "亲儿子",
+          id: 9,
+        },
+        {
+          name: "李四",
+          type: "儿子",
+          id: 10,
+        },
+        {
+          name: "王五",
+          type: "儿子",
+          id: 11,
+        },
+      ],
+    };
+  },
+  methods: {
+    toAddFriends() {
+      uni.navigateTo({
+        url: "../add-friends/add-friends",
+      });
+    },
+    addAll() {
+      console.log(this.selectList.length);
+      console.log(this.list.length);
+      if (this.selectList.length != this.list.length) {
+        this.selectList = [];
+        this.list.forEach((e) => {
+          this.selectList.push(e.id);
+        });
+      } else {
+        this.selectList = [];
+      }
+    },
+    deleteFriends() {},
+    onSelect(index) {
+      let selectIndex = this.selectList.indexOf(index);
+      if (selectIndex > -1) {
+        this.selectList.splice(selectIndex, 1);
+      } else {
+        this.selectList.push(index);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-	.item {
-		display: flex;
-		flex-direction: row;
-		width: 100%;
-		height: 100rpx;
-		margin: 0 24rpx;
-		border-bottom: 1rpx solid #fefefe;
+page {
+  background: #fefefe;
+}
+.item {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100rpx;
+  border-bottom: 10rpx solid red;
+  background: #ffffff;
+  margin-top: 20rpx;
+}
 
-	}
+.add-friends {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: yellow;
+}
 
-	.add-friends {
-		position: fixed;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		height: 60px;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		background-color: yellow;
-	}
-	.edit-friends{
+.edit-friends {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: yellow;
 
-		position: fixed;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		height: 60px;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		background-color: yellow;
-		.btn{
-			width: 60rpx;
-			height: 60rpx;
-			height: 60rpx;
-			text-align: center;
-			background-color: #555555;
-			margin-left: 200rpx;
-		}
-	}
+  .btn {
+    width: 60rpx;
+    height: 60rpx;
+    height: 60rpx;
+    text-align: center;
+    background-color: #555555;
+    margin-left: 200rpx;
+  }
+}
 </style>
