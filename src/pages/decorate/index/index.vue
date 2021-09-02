cddfd057235215acd94a6951ad95e28f4e020c2d
wangbinyan <1159018192@qq.com>
2021-09-02 20:55:44 +0800

    feat: 装修量房首页

--- src/pages/decorate/index/index.vue
+++ src/pages/decorate/index/index.vue
@@ -1,136 +1,92 @@
 <template>
-	<view class="content">
-		<view class="v1">
-			<text>装修服务</text>
-			<view @click="goToMyDecorate">></view>
-		</view>
-		<view class="design-picture">
-			<image @click="goDesignPicture" mode="aspectFit"
-				src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fclubfiles.liba.com%2F2006%2F08%2F23%2F15%2F4603714.jpg&refer=http%3A%2F%2Fclubfiles.liba.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632561406&t=2224059e9c261a09a68eaffa2d2e371d">
-			</image>
-		</view>
-<!--    <button @click="toSend">发送消息</button>
-    <text>{{mqttMessage}}</text> -->
-		<decorate-notice v-if="noticeActive" @closeNotice='closeNotice' class="decorate-notice"></decorate-notice>
-		<drag-button-follow :style.sync="style" @btnClick='noticeActive=true' :follow='`left,right`'
-			className="drag-button" class="drag-button">
-			<view>
-				<text>消息</text>
-				<text style="color: red;">2</text>
-			</view>
-		</drag-button-follow>
-		<view>
-			<view class="v1"><text>量房</text> <text>量房员抢单中</text></view>
-			<view>精算服务</view>
-			<view>设计服务</view>
-			<view @click="gonohouse">无房屋无服入口</view>
-			<view @click="gonohousedecatore">无房屋无服务装修</view>
-			<view @click="gonohousecheck">无房屋无服务验房</view>
-			<view @click="gonohavehouse">有房屋无服务首页</view>
-		</view>
+	<view class="decorate-index">
+		<image class="bg" mode="aspectFit" src="../../../static/bg@2x.png"></image>
+		<view class="content">
+			<view class="house-firend">
+				<view class="title">
+					<view class="house" @click="switchVisible">
+						<text>我的家</text>
+						<image class="ic-triangle" src="../../../static/images/ic_triangle.svg"></image>
+					</view>
+					<view class="friend">
+						<text>亲友团</text>
+						<view class="friend-list">
+							<image class="avtor" v-for="(item, index) in friendList" :key="item.id"
+								:src="item.relativeAvatar"
+								:class="{avtor1: index == 0, avtor1: index == 0,avtor2: index == 1}"></image>
+							<view class="avtor-more" @click="toFriends">
+								<image src="../../../static/svg/ic_avtor_more.svg"></image>
+							</view>
+						</view>
+					</view>
+				</view>
 
+				<!-- 切换房屋弹窗 -->
+				<uni-popup ref="sw">
+					<house-switch class="margintop" :datalist="myHouseList" :current="current" @goAddHouse="addHouse"
+						@checkHouse="checkHouse"></house-switch>
+				</uni-popup>
+
+				<view class="uni-padding-wrap">
+					<view class="uni-title">{{ currentHouse.housingEstate }}{{currentHouse.address}}</view>
+					<view class="design" @click="goDesignPicture">
+						<text>设计图</text>
+						<image src="../../../static/images/ic_triangle_999.svg"></image>
+					</view>
+				</view>
+
+			</view>
+			<view class="my-decorate-service-wrap">
+				<image mode="aspectFit" class="top-bg" src="../../../static/svg/service-card-top.svg"></image>
+				<view class="my-decorate-service">
+					<view class="service-title flex-space-between-row" @click="goToMyDecorate">
+						<text class="t">我的装修服务</text>
+						<view class="r flex-start-row">
+							<text>查看全部</text>
+							<image src="../../../static/svg/ic_more.svg"></image>
+						</view>
+					</view>
+					<service-item :status="DECTORE_DICT.robbing" itemName="量房服务" statusName="待确认量房员"></service-item>
+					<service-item :status="DECTORE_DICT.shouldsure" itemName="验房服务" statusName="验房员抢单中"></service-item>
+					<service-item :status="DECTORE_DICT.inservice" itemName="设计服务" statusName="设计师抢单中"></service-item>
+					<service-item :status="DECTORE_DICT.uncheck" itemName="精算服务" statusName="待确认精算师"></service-item>
+				</view>
+			</view>
+
+			<decorate-notice v-if="noticeActive" @closeNotice='closeNotice' class="decorate-notice"></decorate-notice>
+			<drag-button-follow :style.sync="style" @btnClick='noticeActive=true' :follow='`left,right`'
+				className="drag-button" class="drag-button">
+				<view>
+					<text>消息</text>
+					<text style="color: red;">2</text>
+				</view>
+			</drag-button-follow>
+			<!-- <view class="link">
+				<button @click="gonohouse">无房屋无服入口</button>
+				<button @click="gonohousedecatore">无房屋无服务装修</button>
+				<button @click="gonohousecheck">无房屋无服务验房</button>
+				<button @click="gonohavehouse">有房屋无服务首页</button>
+				<button @click="gonohavehouse">有房屋无服务首页</button>
+			</view> -->
+
+		</view>
 	</view>
 </template>
 
 <script>
 	import {
-		queryEstates
+		queryEstates,
+		friendListByEstateId
 	} from "../../../api/decorate.js";
-
+	import {
+		HouseSwitch
+	} from "../../../components/house-switch/house-switch.vue"
+	import ServiceItem from "../../../components/service-item/service-item.vue"
+	import { DECTORE_DICT } from "../../../utils/dict.js"
 	export default {
-    onLoad() {
-      // // console.log(connect)
-      // //注意小程序切换到后台时可能会断开连接，需要重连
-      // var mqtt = require('mqtt/dist/mqtt.js')
-      // const url = 'wxs://post-cn-tl32ajx3u0l.mqtt.aliyuncs.com';
-      // // console.log(url)
-      // const client = mqtt.connect(url, {
-      // 	// 8位16进制的随机值组成clientId
-      //   keepalive: 0,
-      // 	clientId: this.clientId,
-      //   username:this.username,
-      //   password:this.password,
-      //   connectTimeout: 5000,
-      // });
-
-      // this.client = client
-      // client.on('connect', function(connack){
-      //   console.log('连接中')
-      // 	const { cmd, returnCode } = connack;
-      //   console.log( cmd, returnCode)
-      // 	// 返回的命令为connack值，且 returnCode为0，则表示连接被接受，成功
-      // 	if (cmd === 'connack' && returnCode === 0) {
-      // 		// @todo 连接成功
-      // 		console.log('连接成功');
-      //     client.subscribe('dabanjia/testTopic',{qos: 1}, function(err) {
-      //       console.log(err)
-      //       if (!err) {
-      //         console.log(111)
-      //         // client.publish('dabanjia/testTopic', 'hello mqtt')
-      //       }
-      //     })
-      // 	} else {
-      // 		// @todo 连接失败
-      //     console.log('失败')
-      // 		return;
-      // 	}
-
-      // 	// 以下各监听事件的绑定，必须在连接成功后才可以做
-
-      // 	// 监听消息事件
-      // 	client.on('message', (topic, message, error) => {
-      //     console.log('message',message)
-      //     let encodedString = String.fromCharCode.apply(null, new Uint8Array(message));
-      //     console.log('内容'+encodedString)
-      // 		if (!error) {
-      // 			// @todo 成功接收到消息
-      //       console.log('成功')
-      // 			console.log(topic, message.toString());
-      // 		} else {
-      //       console.log('失败')
-      // 			console.error(error);
-      // 		}
-      // 	});
-
-      // 	// 监听关闭事件
-      // 	client.on('close', () => {
-      // 		console.log('连接已关闭');
-      // 	});
-
-      // 	// 订阅话题，可以订1个或多个
-      // 	// @topic String | Array
-      // 	// client.subscribe('dabanjia/testTopic');
-
-      // 	// 发布消息，话题必须有且只能一个
-      // // 	client.publish('testtopic', 'hello', (error) => {
-      // // 		if (!error) {
-      // // 			// @todo 发布成功
-
-      // // 		} else {
-      // // 			// @todo 发布失败
-
-      // // 		}
-      // // 	});
-      // });
-      //   client.on('reconnect', (error) => {
-      //    console.log('正在重连:', error)
-      //   })
-
-      //   client.on('error', (error) => {
-      //    console.log('连接失败:', error)
-      //   })
-      //   // 断开
-      //   client.on('end', () => {
-      //     console.log('end')
-      //   });
-      //   // 掉线
-      //   client.on('offline', (msg) => {
-      //     console.log('offline',msg)
-      //   });
-    },
-		mounted() {
-
-    },
+		components: {
+			HouseSwitch, ServiceItem
+		},
 		onShow() {
 			if (this.houses && this.houses.length < 1) {
 				this.getHouses();
@@ -141,27 +97,98 @@
 				style: "",
 				noticeActive: false,
 				houses: getApp().globalData.houses,
-        accessKeyId:'LTAI5tKwuhb948v9oakqnbTf',
-        instanceId:'post-cn-tl32ajx3u0l',
-        groupId:'GID_dabanjia',
-        deviceId:`mqttjs_${Math.random().toString(16).substr(2, 8)}`,
-        token:'',
-        client:{}
+				accessKeyId: 'LTAI5tKwuhb948v9oakqnbTf',
+				instanceId: 'post-cn-tl32ajx3u0l',
+				groupId: 'GID_dabanjia',
+				deviceId: `mqttjs_${Math.random().toString(16).substr(2, 8)}`,
+				token: '',
+				client: {},
+				currentHouse: {},
+				myHouseList: [],
+				current: null,
+				friendList: [], 
+				DECTORE_DICT,
 			};
 		},
-    computed:{
-      username(){
-        return`Token|${this.accessKeyId}|${this.instanceId}`
-      },
-      //token和设备id关联，需要后端接口提供
-      password(){
-        return `R|LzMT+XLFl5s/YWJ/MlDz4t/Lq5HC1iGU1P28HAMaxYzmBSHQsWXgdISJ1ZJ+2cxaamjCkkdmS/XOGd160KYNICpRDnjsfBujbJGYgJWUr5piesdvDY0i8S48f1y+kDSyD1qZq3RLscnvooOIjF1CZUnSLi/oIC4juK1MZ8qVI7uIdBoQzt4TbiQgoJWL8b3AQUS1QPxDA2oGf+JBKuN0DyYW6d7mIYhAqXTpVbQw5nNCvKP80Xo0WQLnbM+hoyCSPOmGbPwAsaS1bd9VJjqDoJlCt6GFmJgm2JFY7PJwf/7OOSmUYIYFs5o/PuPpoTMF+hcVXMs+0yDukIMTOzG9m1KmYYo48q4Eb41jz5zvCIjTrIiblxfX1Q==|W|LzMT+XLFl5s/YWJ/MlDz4t/Lq5HC1iGU1P28HAMaxYzmBSHQsWXgdISJ1ZJ+2cxaamjCkkdmS/XOGd160KYNICpRDnjsfBujbJGYgJWUr5piesdvDY0i8S48f1y+kDSyD1qZq3RLscnvooOIjF1CZUnSLi/oIC4juK1MZ8qVI7uIdBoQzt4TbiQgoJWL8b3AQUS1QPxDA2oGf+JBKuN0DyYW6d7mIYhAqXTpVbQw5nNCvKP80Xo0WQLnbM+hoyCSPOmGbPwAsaS1bd9VJjqDoJlCt6GFmJgm2JFY7PJwf/7OOSmUYIYFs5o/PuPpoTMF+hcVXMs+0yDukIMTOzG9m1KmYYo48q4Eb41jz5zvCIjTrIiblxfX1Q==`
-      },
-      clientId(){
-        return `${this.groupId}@@@1234`
-      }
-    },
+		mounted() {
+			this.getMyHouseList();
+		},
+		computed: {
+			username() {
+				return `Token|${this.accessKeyId}|${this.instanceId}`
+			},
+			//token和设备id关联，需要后端接口提供
+			password() {
+				return `R|LzMT+XLFl5s/YWJ/MlDz4t/Lq5HC1iGU1P28HAMaxYzmBSHQsWXgdISJ1ZJ+2cxaamjCkkdmS/XOGd160KYNICpRDnjsfBujbJGYgJWUr5piesdvDY0i8S48f1y+kDSyD1qZq3RLscnvooOIjF1CZUnSLi/oIC4juK1MZ8qVI7uIdBoQzt4TbiQgoJWL8b3AQUS1QPxDA2oGf+JBKuN0DyYW6d7mIYhAqXTpVbQw5nNCvKP80Xo0WQLnbM+hoyCSPOmGbPwAsaS1bd9VJjqDoJlCt6GFmJgm2JFY7PJwf/7OOSmUYIYFs5o/PuPpoTMF+hcVXMs+0yDukIMTOzG9m1KmYYo48q4Eb41jz5zvCIjTrIiblxfX1Q==|W|LzMT+XLFl5s/YWJ/MlDz4t/Lq5HC1iGU1P28HAMaxYzmBSHQsWXgdISJ1ZJ+2cxaamjCkkdmS/XOGd160KYNICpRDnjsfBujbJGYgJWUr5piesdvDY0i8S48f1y+kDSyD1qZq3RLscnvooOIjF1CZUnSLi/oIC4juK1MZ8qVI7uIdBoQzt4TbiQgoJWL8b3AQUS1QPxDA2oGf+JBKuN0DyYW6d7mIYhAqXTpVbQw5nNCvKP80Xo0WQLnbM+hoyCSPOmGbPwAsaS1bd9VJjqDoJlCt6GFmJgm2JFY7PJwf/7OOSmUYIYFs5o/PuPpoTMF+hcVXMs+0yDukIMTOzG9m1KmYYo48q4Eb41jz5zvCIjTrIiblxfX1Q==`
+			},
+			clientId() {
+				return `${this.groupId}@@@1234`
+			}
+		},
 		methods: {
+			async getFriendsList() {
+				let list = await friendListByEstateId({
+					estateId: this.currentHouse.id
+				});
+				this.friendList = list.length > 2 ? list.slice(0, 2) : list
+			},
+			toFriends() {
+				uni.navigateTo({
+					url: "../../decorate/friends/friends?id=" + this.currentHouse.id,
+				});
+			},
+			addHouse() {
+				uni.navigateTo({
+					url: "/pages/decorate/add-house/add-house"
+				})
+			},
+			checkHouse(item) {
+				this.current = item.id
+				this.currentHouse = item
+				this.$refs.sw.close()
+			},
+			getMyHouseList() {
+				queryEstates({
+					isNeedRelative: true
+				}).then(data => {
+					let i = 1;
+					let names = ["设计阶段", "未开工", "已竣工"]
+					for (let item of data) {
+						item.statusName = names[i - 1]
+						item.status = i++
+					}
+					data[1].ext = "第二次装修"
+					data[2].friend = true
+					data[2].ext = "首次装修"
+					this.myHouseList = data
+					const arr = data.filter(t => t.defaultEstate)
+					// let temp = null;
+					// if(arr.length > 0) {
+					// 	temp = arr[0]
+					// } else {
+					// 	data[0].defaultEstate = true
+					// 	temp = data[0]
+					// }
+					// this.currentHouse = temp
+					// this.current = temp.id
+					this.currentHouse = arr[0]
+					this.current = arr[0].id
+					if (arr[0].id) {
+						this.getFriendsList()
+					}
+				})
+			},
+			bindChange(e) {
+				console.log(e)
+			},
+			switchVisible() {
+				this.$refs.sw.open('top')
+			},
+			goDesignPicture() {
+				uni.navigateTo({
+					url: "/pages/decorate/design-picture/design-picture"
+				})
+			},
 			gonohousedecatore() {
 				uni.navigateTo({
 					url: "/pages/decorate/no-house-decorate/no-house-decorate"
@@ -182,10 +209,10 @@
 					url: "/pages/decorate/no-house/no-house"
 				})
 			},
-      toSend(){
-        console.log('发送消息')
-        this.client.publish('dabanjia/testTopic', 'hello zzz')
-      },
+			toSend() {
+				console.log('发送消息')
+				this.client.publish('dabanjia/testTopic', 'hello zzz')
+			},
 			closeNotice() {
 				this.noticeActive = false;
 			},
@@ -226,20 +253,241 @@
 </script>
 
 <style lang="scss" scoped>
+	.decorate-index {
+		position: relative;
+
+		.bg {
+			width: 100%;
+		}
+	}
+
 	.content {
 		width: 100%;
 		height: 100%;
 		position: relative;
+		margin-top: -304rpx;
 	}
 
-	.v1 {
+	.house-firend {
+		position: relative;
+
+		.title {
+			display: flex;
+			justify-content: space-between;
+			flex-direction: row;
+			align-items: center;
+			margin-bottom: 4rpx;
+		}
+
+		.house {
+			display: flex;
+			justify-content: flex-start;
+			flex-direction: row;
+			align-items: center;
+			margin-left: 40rpx;
+
+			.ic-triangle {
+				width: 13rpx;
+				height: 8rpx;
+			}
+
+			text {
+				width: 126rpx;
+				height: 56rpx;
+				font-size: 40rpx;
+				font-family: PingFangSC, PingFangSC-Medium;
+				font-weight: 500;
+				text-align: left;
+				color: #333333;
+				line-height: 56rpx;
+				letter-spacing: 2rpx;
+				margin-right: 9rpx;
+			}
+		}
+
+		.friend {
+			margin-right: 32rpx;
+			display: flex;
+			justify-content: flex-start;
+			flex-direction: row;
+			align-items: center;
+
+			>text {
+				height: 40rpx;
+				font-size: 24rpx;
+				font-family: PingFangSC, PingFangSC-Regular;
+				font-weight: 400;
+				text-align: right;
+				color: #666666;
+				line-height: 40rpx;
+				margin-right: 16rpx;
+			}
+
+			.friend-list {
+				display: flex;
+				justify-content: flex-start;
+				flex-direction: row;
+				align-items: center;
+
+				.avtor {
+					width: 40rpx;
+					height: 40rpx;
+					border-radius: 50%;
+				}
+
+				.avtor2,
+				.avtor-more {
+					margin-left: -12rpx;
+				}
+
+				.avtor2 {
+					z-index: 9;
+				}
+
+				.avtor-more {
+					box-sizing: border-box;
+					width: 40rpx;
+					height: 40rpx;
+					background: #f5f6f6;
+					border: 1rpx solid #ffffff;
+					border-radius: 50%;
+					z-index: 99;
+
+					image {
+						width: 20rpx;
+						height: 4rpx;
+						margin: 13rpx 10rpx;
+					}
+				}
+			}
+		}
+
+		.picker-view {
+			width: 750rpx;
+			height: 600rpx;
+			margin-top: 20rpx;
+		}
+
+		.item {
+			height: 50px;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+		}
+
+		.uni-padding-wrap {
+			margin-left: 40rpx;
+			height: 182rpx;
+
+			.uni-title {
+				// width: 314rpx;
+				height: 34rpx;
+				font-size: 24rpx;
+				font-family: PingFangSC, PingFangSC-Regular;
+				font-weight: 400;
+				text-align: left;
+				color: #999999;
+				line-height: 34rpx;
+				margin-bottom: 40rpx;
+			}
+
+			.design-wrap {
+				// margin-left: 40rpx;
+			}
+		}
+
+		.design {
+			display: flex;
+			justify-content: center;
+			flex-direction: row;
+			align-items: center;
+			width: 114rpx;
+			height: 44rpx;
+			background: #f5f6f6;
+			border-radius: 12rpx;
+
+			text {
+				height: 32rpx;
+				font-size: 22rpx;
+				font-family: PingFangSC, PingFangSC-Regular;
+				font-weight: 400;
+				text-align: left;
+				color: #333333;
+				line-height: 32rpx;
+				margin-right: 8rpx;
+			}
+
+			image {
+				height: 13rpx;
+				width: 8rpx;
+			}
+		}
+	}
+
+	.my-decorate-service-wrap {
+		position: relative;
+		margin: 0 24rpx;
+		background: #ffffff;
+		border-radius: 24rpx;
+		box-shadow: 0rpx 2rpx 20rpx 0rpx rgba(0, 0, 0, 0.05);
+		min-height: 256rpx;
+	}
+	.my-decorate-service {
+		padding: 0 24rpx 24rpx;
+		position: relative;
+	}
+
+	.top-bg {
+		width: 100%;
+		height: 120rpx;
+	}
+	.flex-space-between-row {
 		display: flex;
-		flex-direction: row;
 		justify-content: space-between;
-		margin-bottom: 30rpx;
-		border: 2rpx solid green;
+		align-items: center;
+		flex-direction: row;
+	}
+	.flex-start-row {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		flex-direction: row;
+	}
+	.service-title {
+		margin: -88rpx 45rpx 44rpx 32rpx;
+		.t {
+			font-size: 32rpx;
+			font-family: PingFangSC, PingFangSC-Medium;
+			font-weight: 500;
+			text-align: left;
+			color: #333333;
+			letter-spacing: 1rpx;
+		}
+		.r {
+			text {
+				height: 36rpx;
+				margin-right: 16rpx;
+				font-size: 26rpx;
+				font-family: PingFangSC, PingFangSC-Regular;
+				font-weight: 400;
+				text-align: right;
+				color: #999999;
+				line-height: 36rpx;
+			}
+			image {
+				width: 11rpx;
+				height: 19rpx;
+			}
+		}
 	}
 
+	// <view class="service-title">
+	// 	<text class="t">我的装修服务</text>
+	// 	<text class="r">
+	// 		<text>查看全部</text>
+	// 		<image src="../../../static/svg/ic_more.svg"></image>
+	// 	</text>
+	// </view>
 	.drag-button {
 		background: #ffffff;
 		border: 0.5px solid #eeeeee;
@@ -257,12 +505,21 @@
 		justify-content: center;
 	}
 
-	.design-picture {
-		border: 2rpx solid green;
+	.link {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		flex-wrap: wrap;
 
-		image {
-			width: 250rpx;
-			height: 250rpx;
+		button {
+			display: inline-block;
+			line-height: 1;
+			padding: 10rpx 6rpx;
+			border: 2rpx solid green;
+			color: #fff;
+			background-color: #000088;
+			margin: 10rpx;
+			font-size: 24rpx;
 		}
 	}
 </style>
