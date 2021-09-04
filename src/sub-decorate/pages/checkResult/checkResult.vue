7d5bfb42cf935fb5d8069b6c79354d0921322088
liuyubo <365768924@qq.com>
2021-09-04 12:29:02 +0800

    feat(#装修):验房报告详情开发

--- src/sub-decorate/pages/checkResult/checkResult.vue
+++ src/sub-decorate/pages/checkResult/checkResult.vue
@@ -2,26 +2,38 @@
   <view class="check-result">
     <view class="report-content">
       <view class="report-title">
-        <text class="title">验房报告</text>
-        <text class="time">2021-08-21 13:00:00</text>
+        <view>
+          <text class="title">验房报告</text>
+          <text class="time">2021-08-21 13:00:00</text>
+        </view>
       </view>
       <view class="charts">
         <uniEcCanvas class="uni-ec-canvas" id="uni-ec-canvas" ref="canvas" canvas-id="uni-ec-canvas" :ec="ec">
         </uniEcCanvas>
       </view>
       <text class="report-text">这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第一个版本可支持语音和视频这里的文字是识别出来的。这里展示验房人员在总结中的文字稿，不可隐藏，所有的内容都展示。第一都展都展</text>
+      <view class="img-list">
+        <image></image>
+        <image></image>
+        <image></image>
+      </view>
+      
     </view>
+    <deliverCard color="#CA3737" title="重大隐患" :data='data[2]'></deliverCard>
+    <deliverCard color="#F6A93B" title="隐患" :data='data[1]'></deliverCard>
+    <deliverCard color="#348BE2" title="符合项" :data='data[0]'></deliverCard>
   </view>
 </template>
 
 <script>
   import uniEcCanvas from '../../components/uni-ec-canvas/uni-ec-canvas.vue'
   import * as echarts from '../../components/uni-ec-canvas/echarts'
+  import deliverCard from '../../components/delivery-card/delivery-card.vue'
   let chart = null
-
   export default {
     components: {
-      uniEcCanvas
+      uniEcCanvas,
+      deliverCard
     },
     data() {
       return {
@@ -103,23 +115,51 @@
             name: '符合项',
             itemStyle: {
               color: '#348BE2'
-            }
-
+            },
+            arr:[
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²，且每个自然间不多于2处",
+                content:"这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+            ]
           },
           {
             value: 12,
             name: '隐患',
             itemStyle: {
               color: '#F6A93B'
-            }
+            },
+            arr:[
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²，且每个自然间不多于2处",
+                content:"这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²，且每个自然间不多于2处",
+                content:"这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+            ]
           },
           {
             value: 8,
             name: '重大隐患',
             itemStyle: {
               color: '#CA3737'
-            }
-
+            },
+            arr:[
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²，且每个自然间不多于2处",
+                content:"这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²，且每个自然间不多于2处",
+                content:"这里展示验房人员在总结中的文字稿，不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+              {
+                title:"满敲墙面，每处空鼓面积≤0.04m²",
+                content:"不可隐藏所有的内容都展示。第版支持语音和视手机都可能发生了咖啡那是开发那开发哪上课"
+              },
+            ]
           },
         ],
         num: 43
@@ -144,7 +184,6 @@
         }
       },
       initChart(canvas, width, height, canvasDpr) {
-        console.log(canvas, width, height, canvasDpr)
         chart = echarts.init(canvas, null, {
           width: width,
           height: height,
@@ -163,25 +202,67 @@
   .check-result {
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
+    padding: 0 24rpx;
   }
 
+  .report-title{
+    height: 162rpx;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #efefef;
+    .title{
+      color: #333333;
+      font-size: 40rpx;
+      font-weight: 500;
+      letter-spacing: 1px;
+      // margin-bottom: 8rpx;
+      display: block;
+    }
+    .time{
+      font-weight: 400;
+      font-size: 24rpx;
+      color: #666666;
+    }
+  }
+  
   .report-content {
-    width: 638rpx;
+    // width: 638rpx;
+    padding: 32rpx 32rpx 40rpx;
     // height: 608px;
     opacity: 1;
     background: #ffffff;
     border-radius: 24rpx;
-    padding: 32rpx;
+    .report-text{
+      color: #333;
+      font-size: 26rpx;
+      font-weight: 400;
+      letter-spacing: 1px;
+    }
+    .img-list{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      margin-top: 40rpx;
+      image{
+        width: 208rpx;
+        height: 208rpx;
+        opacity: 1;
+        border-radius: 8rpx;
+        // border: 1px solid #eee;
+        background-color: #eee;
+      }
+    }
   }
 
   .charts {
     height: 350rpx;
     width: 100%;
-
     .uni-ec-canvas {
       height: 100%;
       width: 100%;
       display: block;
     }
   }
+  
 </style>
---
+++ src/sub-decorate/pages/result-detail/result-detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    213321
+  </view>
+</template>
+
+<script>
+  export default{
+    data(){}
+  }
+</script>
+
+<style>
+</style>
