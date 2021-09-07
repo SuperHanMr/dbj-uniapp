50169921ddd84ce8f8beb6356812e682c9125d2e
liuyubo <365768924@qq.com>
2021-09-07 15:30:30 +0800

    feat(#装修):新增我的花销

---
+++ src/sub-decorate/pages/current-cost/current-cost.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="current-cost">
+    <view class="charts-image"></view>
+    
+    <cost-item></cost-item>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
