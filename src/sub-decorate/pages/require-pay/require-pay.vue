6b2ff836a03ea4d698120923a1d0bc62db48d228
贺成龙 <mr_dragonn@foxmail.com>
2021-09-08 22:00:15 +0800

    修改真机时tabbar不显示

--- src/sub-decorate/pages/require-pay/require-pay.vue
+++ src/sub-decorate/pages/require-pay/require-pay.vue
@@ -4,14 +4,24 @@
 			<view class="header">
 				{{item.title}}
 			</view>
-			<view class="items" v-for="(sub,subIndex) in item.children">
+			<view class="items" v-for="(sub,subIndex) in item.children" :key="subIndex">
 				<image class="img" src="" mode=""></image>
 				<view class="content">
 					<view class="title">
+						<text class="tip"></text>
 						{{sub.title}}
 					</view>
+					<view>
+						<view class="spec">
+							管径DN25 3.5 绿
+						</view>
+					</view>
+
 					<view class="sub-title">
 						{{sub.sub}}
+					</view>
+					<view class="price">
+						99.88/根
 					</view>
 				</view>
 			</view>
@@ -34,7 +44,23 @@
 				</view>
 			</view>
 		</view>
-		<bottom-btn btnContent="确认要货" @submit="submit"></bottom-btn>
+		<view style="height: 200rpx;">
+
+		</view>
+		<bottom-btn :showDefaultBtn="false" bgcolor="#FFF">
+			<template v-slot:default>
+				<view class="reject-btn">
+					拒绝申请
+				</view>
+				<view style="flex: 1;">
+
+				</view>
+				<view class="agree-btn">
+					同意并支付 ¥ 460.00
+				</view>
+			</template>
+
+		</bottom-btn>
 	</view>
 </template>
 
@@ -84,6 +110,52 @@
 </script>
 
 <style lang="scss" scoped>
+	.tip {
+		display: inline-block;
+	}
+
+	.spec {
+		display: inline-block;
+		background: #fafafa;
+		border: 1rpx solid #f0f0f0;
+		border-radius: 6rpx;
+		padding: 6rpx 12rpx;
+		font-size: 22rpx;
+		color: #999999;
+
+	}
+
+	.price {
+		color: #333333;
+		font-size: 24rpx;
+
+	}
+
+	.reject-btn {
+		width: 188rpx;
+		height: 88rpx;
+		line-height: 88rpx;
+		opacity: 1;
+		border: 1rpx solid #cccccc;
+		border-radius: 16rpx;
+		text-align: center;
+		color: #666666;
+		font-size: 30rpx;
+		margin-left: 32rpx;
+	}
+
+	.agree-btn {
+		width: 466rpx;
+		height: 88rpx;
+		line-height: 88rpx;
+		text-align: center;
+		color: #ffffff;
+		font-size: 30rpx;
+		background: linear-gradient(135deg, #00bfaf, #00bfbc);
+		border-radius: 16rpx;
+		margin-right: 32rpx;
+	}
+
 	.store {
 		padding: 0 32rpx;
 		background-color: #FFF;
@@ -113,13 +185,14 @@
 					-webkit-box-orient: vertical;
 					overflow: hidden;
 					text-overflow: ellipsis;
-					-webkit-line-clamp: 2;
+					-webkit-line-clamp: 1;
 				}
 
 				.sub-title {
 					margin-top: 16rpx;
 					font-size: 22rpx;
 					color: #999999;
+					text-align: end;
 				}
 			}
 		}
@@ -132,7 +205,7 @@
 			color: #333333;
 			font-size: 28rpx;
 			font-weight: 500;
-margin-bottom: 32rpx;
+			margin-bottom: 32rpx;
 		}
 	}
 
