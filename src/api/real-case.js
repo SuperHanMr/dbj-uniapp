b551e643a47c1064a58437a7783bdbb09a10e882
caonaCoding <caona1173146358@163.com>
2021-09-15 15:50:18 +0800

    update api data

--- src/api/real-case.js
+++ src/api/real-case.js
@@ -12,7 +12,19 @@ export function getServeTypes(projectId){
 export function getDrawings(params){
   return request.get(`/pm/app/serve/design/designServeTypeInfo`,{params});
 }
+export function updateDrawings(severId){
+  return request.get(`/pm/app/serve/design/designServeFile/${severId}`,{});
+}
 export function getDecorateDynamic(params){
   return request.post(`/pm/app/decorate/calendar/getListByPageQuery`,params);
+}
+export function getSelectOptions(params){
+  return request.get(`/pm/app/project/myService`,{params});
+}
+export function setAttentions(params){
+  return request.post(`/app/base/interactive/add`,params);
+}
+export function getFocusBrowse(params){
+  return request.get(`/app/base/interactive/project/count/detail`,{params});
 }
 
