6b24e4b55f2ef2b8fcdfd6212bd22ad7e7805288
xuqingsong <xqsy77@163.com>
2021-09-17 19:02:34 +0800

    冲突 徐青松

--- src/api/real-case.js
+++ src/api/real-case.js
@@ -7,9 +7,6 @@ export function getcollection(params) {
   return request.post(`/app/base/interactive/add`, params);
 }
 export function getCaseList(params) {
-  return request.get(`/app/case/v2/list`, { params });
-}
-export function getCaseList(params) {
   return request.get(`/app/case/v2/list`, {
     params
   });
