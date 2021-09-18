0538fb0583490c09e34b5effcf8604552d7add32
caonaCoding <caona1173146358@163.com>
2021-09-18 10:19:02 +0800

    购物车结算跳转至确认订单页传参数处理

--- src/sub-my/pages/shopping-cart/shopping-cart.vue
+++ src/sub-my/pages/shopping-cart/shopping-cart.vue
@@ -506,7 +506,10 @@
 					uni.navigateTo({
 						url:"./confirm-order",
 						success: (res) => {
-							res.eventChannel.emit('acceptDataFromOpenerPage',checkedList)
+							res.eventChannel.emit('acceptDataFromOpenerPage',{
+								skuInfos: checkedList,
+								originFrom: "shopCart",
+							})
 						}
 					})
 				}else{
@@ -535,12 +538,28 @@
 								if(ele.storeId === item.storeId){
 									ele.storeName = item.storeName
 								}
-								checkedList.push(ele)
+								checkedList.push({
+									skuId: ele.skuId,
+									storeId: ele.storeId,
+									buyCount: ele.buyCount,
+									unit: ele.unitName,
+									level: 0
+								})
 								//根据productType商品类型划分为服务类和实物类
 								if(ele.productType === 1){
-									this.serviceList.push(ele)
+									this.serviceList.push({
+										storeId: ele.storeId,
+										buyCount: ele.buyCount,
+										unit: ele.unitName,
+										level: 0
+									})
 								}else{
-									this.entityList.push(ele)
+									this.entityList.push({
+										storeId: ele.storeId,
+										buyCount: ele.buyCount,
+										unit: ele.unitName,
+										level: 0
+									})
 								}
 							}
 						})
