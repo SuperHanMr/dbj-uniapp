ba46e30f889dafc87384f2885a62d8a0e82dd66c
liys <lysd163186@163.com>
2021-09-22 15:03:34 +0800

    调整订单

--- src/sub-classify/pages/goods-detail/goods-detail.vue
+++ src/sub-classify/pages/goods-detail/goods-detail.vue
@@ -28,8 +28,8 @@
     },
     onShow() {
       this.refreshHash = Math.random()
-      // this.baseUrl = this.ENV.VUE_APP_BASE_H5
-      this.baseUrl = "https://local.meiwu365.com" 
+      this.baseUrl = this.ENV.VUE_APP_BASE_H5
+      // this.baseUrl = "https://local.meiwu365.com" 
       if(uni.getStorageSync('goodId')) {
         this.goodId = uni.getStorageSync('goodId')
       }else if( uni.getStorageSync('fromShopCart')) {
