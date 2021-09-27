45d814f732ac7363cf37754db460a4d851355e67
liys <lysd163186@163.com>
2021-09-27 14:45:56 +0800

    商品详情

---
+++ src/sub-classify/pages/shops-detail/shops-detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <view>
+     <web-view :src="baseUrl + '/app-pages/shops-detail/index.html'"></web-view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        baseUrl: '',
+        token: getApp().globalData.token
+      }
+    },
+    onShow() {
+      // this.baseUrl = this.ENV.VUE_APP_BASE_H5
+      this.baseUrl = "https://local.meiwu365.com" 
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
