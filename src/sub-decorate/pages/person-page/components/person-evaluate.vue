66c07cae57fa2f92f88b23667a5951bf1dbb7b36
liuyubo <365768924@qq.com>
2021-09-27 21:28:40 +0800

    feat(#个人主页):ui开发以及接口联调

--- src/sub-decorate/pages/person-page/components/person-evaluate.vue
+++ src/sub-decorate/pages/person-page/components/person-evaluate.vue
@@ -1,109 +1,53 @@
 <template>
   <view class="person-evaluate person-content-item">
     <view class="title">Ta的评价</view>
-    <view class="evaluate-item">
-      <view class="item-top">
-        <view class="name">渴死的鱼</view>
-        <view class="time">2021年5月3日</view>
-      </view>
-      <view class="item-content">
-        <view class="evaluate-list">
-          <image v-for="el of num"  src="../../../static/ic_score_star@2x.png" mode=""></image>
-          <image v-for="el of 5-num"  src="../../../static/ic_blank_star@2x.png" mode=""></image>
-        </view>
-        <view class="evaluate-text">设计师太有才了，超喜欢这种设计设计设两行字显示喜欢这种设计设计设两行字设计设计设两行字设计设计设两行字设计设计设两行字设计设计设两行字设计的设…</view>
-        <view class="image-list">
-          <image-preview :list='list' :row='3'></image-preview>
-        </view>
-        <view class="case-item">
-          <image></image>
-          <text>谁都快疯了的思念疯狂了打给你快乐的你疯狂拉升带你飞</text>
-        </view>
-      </view>
+    <view class="">
+      <personEvaliateItem class="person-item" :last='index===evaluate.list.length-1' v-for="(item,index) in evaluate.list" :item='item'></personEvaliateItem>
     </view>
-    <view class="click-text">查看全部评价<image src="" mode=""></image></view>
+    <view class="click-text" v-if="evaluate.totalPage>1" @click="toEvaluateList">查看全部评价<image src="" mode=""></image></view>
   </view>
 </template>
 
 <script>
   import '../style/common.scss'
+  import personEvaliateItem from './person-evaliate-item.vue'
+  import {getComments} from '@/api/decorate.js'
   export default{
-    props:{},
+    props:{
+      personId:0
+    },
+    components:{
+      personEvaliateItem
+    },
     data(){
       return{
-        num:3
+        num:3,
+        evaluate:{}
+      }
+    },
+    mounted() {
+      this.getComments()
+    },
+    methods:{
+      getComments(){
+        let data = {
+          page:1,
+          rows:2,
+          userId:this.personId||6862
+        }
+        getComments(data).then(res=>{
+          this.evaluate = res
+        })
+      },
+      toEvaluateList(){
+        uni.navigateTo({
+          url:'/sub-decorate/pages/person-page/person-evaluate-list?id='+this.personId
+        })
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .item-top{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 28rpx;
-    .name{
-      font-size: 28rpx;
-      color: #333;
-      font-weight: 500;
-    }
-    .time{
-      font-size: 22rpx;
-      color: #999;
-    }
-  }
-  .item-content{
-    margin-top: 24rpx;
-    margin-bottom: 32rpx;
-    .evaluate-list{
-      margin-bottom: 8rpx;
-      image{
-        width: 30rpx;
-        height: 30rpx;
-        margin-right: 8rpx;
-      }
-    }
-    .evaluate-text{
-      font-size: 28rpx;
-      color: #666;
-      line-height: 44rpx;
-      overflow : hidden;
-      text-overflow: ellipsis;
-      display: -webkit-box;
-      -webkit-line-clamp: 6;
-      -webkit-box-orient: vertical;
-    }
-    .case-item{
-      margin-top: 16rpx;
-      height: 54rpx;
-      // display: flex;
-      // align-items: center;
-      background-color: #F5F6F6;
-      border: 1px solid #edf0f0;
-      border-radius: 6rpx;
-      padding-left: 2rpx;
-      display: flex;
-      align-items: center;
-      max-width: 380rpx;
-      image{
-        width: 48rpx;
-        height: 48rpx;
-        background: #C1C1C1;
-        border-radius: 4rpx;
-        margin-right: 18rpx;
-      }
-      text{
-        display: inline-block;
-        max-width: 296rpx;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        color: #666;
-        font-size: 24rpx;
-      }
-    }
-  }
   
 </style>
