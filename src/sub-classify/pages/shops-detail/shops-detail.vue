b62c14e656f9419a1b8237d41cc55b601edd263f
liys <lysd163186@163.com>
2021-09-27 21:51:45 +0800

    店铺

--- src/sub-classify/pages/shops-detail/shops-detail.vue
+++ src/sub-classify/pages/shops-detail/shops-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-     <web-view :src="baseUrl + '/app-pages/shops-detail/index.html'"></web-view>
+     <web-view :src="baseUrl + '/app-pages/shops-detail/index.html#wx-storeId=' + storeId + '&wx-token=' + token"></web-view>
   </view>
 </template>
 
@@ -9,12 +9,16 @@
     data() {
       return {
         baseUrl: '',
-        token: getApp().globalData.token
+        token: getApp().globalData.token,
+        storeId: 0
       }
     },
+    onLoad(e) {
+      this.storeId = e.storeId
+    },
     onShow() {
-      // this.baseUrl = this.ENV.VUE_APP_BASE_H5
-      this.baseUrl = "https://local.meiwu365.com" 
+      this.baseUrl = this.ENV.VUE_APP_BASE_H5
+      // this.baseUrl = "https://local.meiwu365.com" 
     },
     methods: {
     }
--- src/sub-classify/pages/shops/shops.vue
+++ src/sub-classify/pages/shops/shops.vue
@@ -1,6 +1,8 @@
 <template>
   <view>
-     <web-view :src="baseUrl + '/app-pages/shops/index.html'"></web-view>
+     <web-view :src="baseUrl + '/app-pages/shops/index.html#wx-storeId='
+     + storeId + '&wx-areaId=' + areaId + '&wx-token=' + token">  
+     </web-view>
   </view>
 </template>
 
@@ -9,12 +11,19 @@
     data() {
       return {
         baseUrl: '',
-        token: getApp().globalData.token
+        token: getApp().globalData.token,
+        storeId: 0,
+        areaId: 0
       }
     },
+    onLoad(e){
+      console.log(e, "teste")
+      this.storeId = e.storeId,
+      this.areaId = e.areaId
+    },
     onShow() {
-      // this.baseUrl = this.ENV.VUE_APP_BASE_H5
-      this.baseUrl = "https://local.meiwu365.com" 
+      this.baseUrl = this.ENV.VUE_APP_BASE_H5
+      // this.baseUrl = "https://local.meiwu365.com" 
     },
     methods: {
     }
