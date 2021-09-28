474a7046604e76a2c659129072a400e76ca561c6
李书梦 <2326107425@qq.com>
2021-09-28 19:48:32 +0800

    修复代付款订单支付成功之后需要跳转的页面

--- src/sub-my/pages/my-order/order-wait-pay/order-wait-pay.vue
+++ src/sub-my/pages/my-order/order-wait-pay/order-wait-pay.vue
@@ -208,11 +208,12 @@ export default {
 								icon:"none",
 								duration:1000,
 							})
-							// setTimeout(()=>{
-							// 	uni.navigateTo({
-							// 		url:`../order-success/order-success?type=complete&id=${this.orderNo}`
-							// 	})
-							// },1000)
+							setTimeout(()=>{
+								// 进入云松写的支付成功页面
+								uni.redirectTo({
+									url:`../../../../sub-classify/pages/pay-order/pay-success?id=${this.orderNo}`
+								})
+							},1000)
 						},
 						fail(e) {
 							uni.showToast({
@@ -222,6 +223,7 @@ export default {
 							});
 						},
 					});
+					
 				});
 			}
 			
