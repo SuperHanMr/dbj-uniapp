5f65551e5667ea200b5de71ea8716442255cf9ac
贺成龙 <mr_dragonn@foxmail.com>
2021-10-01 15:27:23 +0800

    修改直播间ui

--- src/sub-home/pages/lives-room/lives-room.vue
+++ src/sub-home/pages/lives-room/lives-room.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="content">
-		<custom-navbar opacity="1" title="as??ASDSA" titleColor="#FFFFFF" bgcolor="#3b3c48">
+	<view class="content" catchtouchmove=“true”>
+		<custom-navbar opacity="1" :title="title" titleColor="#FFFFFF" bgcolor="none">
 			<template v-slot:back>
 				<view @click="toBack">
 					<i class="icon-ic_cancel_white" style="color: white;">
@@ -14,53 +14,107 @@
 		<!-- 占位 -->
 		<view style="height: 10rpx;">
 		</view>
-		<view class="state-bar">
-			<view class="">
+		<view v-if="isLiveing">
+
+			<view class="state-bar">
+				<view class="">
+
+				</view>
+				<view class="state-bar-text">
+					3.23w
+				</view>
+			</view>
+			<live-player class="player" :src="livePreview" autoplay @statechange="statechange" @error="error" />
+
+			<view class="bottom-contain">
+				<!-- 聊天 -->
+				<scroll-view id="scrollview" :scroll-top="scrollTop" class="scorll-chat" scroll-y="true"
+					:scroll-into-view="bottomId">
+					<view class="chat-item-height">
+						<view class="chat-item" v-for="(item,index) in list" :key="index">
+							{{item}}
+						</view>
+					</view>
+				</scroll-view>
+				<!-- 底部功能栏 -->
+				<view class="bottom-send">
+					<view class="input-text">
+						说点什么...
+					</view>
+					<view class="macphone" @click="clickMacphone">
+						<image class="icon_macphone"
+							src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_macphone.png" mode="">
+						</image>
+						连麦
+					</view>
+					<image class="bottom-icon"
+						src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_shopping.png" mode=""
+						@click="showDownload=true">
+					</image>
+
+					<image class="bottom-icon"
+						src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_agree.png" mode=""></image>
+
+				</view>
+				<!-- 底部占位 -->
+				<view class="bottom-placeholder">
+
+				</view>
 
 			</view>
-			<view class="">
-				3.23w
+
+		</view>
+		<view v-else class="unlived">
+			<image class="img" src="http://dbj.dragonn.top/static/mp/dabanjia/images/home/un-liveing.png" mode="">
+			</image>
+			<view class="tips">
+				直播暂未开始，去首页逛逛吧～
+
+			</view>
+			<view class="border">
+
+			</view>
+			<view class="btn" @click="toBack">
+				去逛逛
+
 			</view>
 		</view>
-		<live-player class="player" :src="livePreview" autoplay @statechange="statechange" @error="error" />
+		<view v-if="!isLogin&&isLiveing" class="remark">
+			<view class="flex1">
 
-		<view class="bottom-contain">
+			</view>
+			<view class="remark-btn" @click="toLogin">
+				登录以使用完整功能
+			</view>
 
-			<!-- 聊天 -->
-			<scroll-view id="scrollview" :scroll-top="scrollTop" class="scorll-chat" scroll-y="true"
-				:scroll-into-view="bottomId">
-				<view class="chat-item-height">
+		</view>
+		<view v-if="showDownload" class="alert-app">
+			<view class="content">
+				<view class="img">
+					<view class="close" @click="showDownload=false">
 
+					</view>
 
-					<view class="chat-item" v-for="(item,index) in list" :key="index">
-						{{item}}
+				</view>
+				<view class="text-content">
+					<view class="text-title">
+						请下载APP使用
+					</view>
+					<view class="text-tips text-top">
+						咨询家装问题，去手机应用市场搜索
+					</view>
+					<view class="text-tips">
+						【打扮家】APP与主播进行交流吧
+					</view>
+					<view class="text-btn" @click="showDownload=false">
+						知道了
+
 					</view>
 				</view>
-			</scroll-view>
-			<!-- 底部功能栏 -->
-			<view class="bottom-send">
-				<view class="input-text">
-					说点什么...
-				</view>
-				<view class="macphone" @click="clickMacphone">
-					<image class="icon_macphone"
-						src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_macphone.png" mode="">
-					</image>
-					连麦
-				</view>
-				<image class="bottom-icon"
-					src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_shopping.png" mode=""></image>
-
-				<image class="bottom-icon"
-					src="http://dbj.dragonn.top/%20static/mp/dabanjia/images/home/lives_agree.png" mode=""></image>
 
 			</view>
-			<!-- 底部占位 -->
-			<view class="bottom-placeholder">
-
-			</view>
-
 		</view>
+
 	</view>
 </template>
 
@@ -81,8 +135,12 @@
 				tophight: "",
 				list: [1, 2, 3],
 				scrollTop: 0,
-				count: 0
-
+				count: 0,
+				isLogin: false,
+				imReady: false,
+				title: '',
+				isLiveing: true,
+				showDownload: false
 			};
 		},
 		onLoad(e) {
@@ -109,39 +167,56 @@
 		},
 		onShow() {
 			if (uni.getStorageSync("userId")) {
-				addListener("MESSAGE_RECEIVED", (event) => {
-					let messageList = event.data || [];
-					let systemMessageList = messageList.filter(msg => msg.conversationID === context.state.sysConv
-						.conversationID);
-					if (systemMessageList.length) {
+				this.isLogin = true;
+				if (!this.imReady) {
 
-					}
-				});
-				getSafeTim().then(tim => {
-					tim.joinGroup({
-						groupID: 'group' + this.roomId,
-						type: TIM.TYPES.GRP_AVCHATROOM
-					}).then(imResponse => {
-						tim.getGroupProfile({
+					addListener("MESSAGE_RECEIVED", );
+					getSafeTim().then(tim => {
+						tim.joinGroup({
 							groupID: 'group' + this.roomId,
-							groupCustomFieldFilter: ['extensionArr']
-						}).then(data => {
-							if (data.code == 0) {
-								// extensionArr
-								let groupCustomField = data.data.group.groupCustomField
-								let res = groupCustomField.filter(e => {
-									return e.key == 'extensionArr'
-								})
-								let result = JSON.parse(res[0].value);
-								console.log('XXXXXXXXX');
-								console.log(result);
-							}
-						});
+							type: TIM.TYPES.GRP_AVCHATROOM
+						}).then(imResponse => {
+							tim.getGroupProfile({
+								groupID: 'group' + this.roomId,
+								groupCustomFieldFilter: ['extensionArr']
+							}).then(data => {
+								if (data.code == 0) {
+									this.imReady = true
+									// extensionArr
+									let groupCustomField = data.data.group.groupCustomField
+									let res = groupCustomField.filter(e => {
+										return e.key == 'extensionArr'
+									})
+									let result = JSON.parse(res[0].value);
+									console.log('XXXXXXXXX');
+									console.log(result);
+								}
+							});
+						})
 					})
-				})
+				}
 			}
 		},
 		methods: {
+			messageRecived(event) {
+				let messageList = event.data || [];
+				let systemMessageList = messageList.filter(msg => msg.conversationID === context.state
+					.sysConv
+					.conversationID);
+				if (systemMessageList.length) {
+
+				}
+			},
+			toLogin() {
+				uni.navigateTo({
+					url: '../../../pages/login/login'
+				})
+			},
+			toBack() {
+				uni.navigateBack({
+
+				})
+			},
 			scrollToBottom() {
 				let that = this;
 				let query = uni.createSelectorQuery();
@@ -174,6 +249,156 @@
 </script>
 
 <style lang="scss">
+	.alert-app {
+		position: absolute;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		.content {
+			width: 640rpx;
+			height: 716rpx;
+			background-color: #FFF;
+
+			.img {
+				width: 640rpx;
+				height: 350rpx;
+				background: url('http://dbj.dragonn.top/static/mp/dabanjia/images/home/lives_download.png');
+				background-size: contain;
+				position: relative;
+
+				.close {
+					float: right;
+					width: 64rpx;
+					height: 64rpx;
+					margin-right: 24rpx;
+					margin-top: 24rpx;
+					background: url('http://dbj.dragonn.top/static/mp/dabanjia/images/home/ic_closed_black.png');
+					background-size: contain;
+
+				}
+			}
+
+			.text-content {
+				background-color: #FFF;
+				width: 640rpx;
+				height: 366rpx;
+				border-bottom-right-radius: 24rpx;
+				border-bottom-left-radius: 24rpx;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+
+				.text-title {
+					font-size: 36rpx;
+					font-weight: 500;
+					color: #333333;
+					line-height: 50rpx;
+					margin-top: 40rpx;
+				}
+
+				.text-tips {
+					font-size: 28rpx;
+					color: #999999;
+					line-height: 42rpx;
+				}
+
+				.text-top {
+					margin-top: 8rpx;
+				}
+
+				.text-btn {
+					width: 266rpx;
+					height: 88rpx;
+					line-height: 88rpx;
+					background: linear-gradient(135deg, #00bfaf, #00bfbc);
+					border-radius: 12rpx;
+					text-align: center;
+					font-size: 28rpx;
+					font-weight: 500;
+					color: #ffffff;
+					margin-top: 48rpx;
+
+				}
+			}
+
+
+		}
+	}
+
+	.unlived {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.img {
+			margin-top: 116rpx;
+			width: 280rpx;
+			height: 120rpx;
+		}
+
+		.tips {
+			margin-top: 36rpx;
+			font-size: 32rpx;
+			font-weight: 500;
+			text-align: center;
+			color: #ffffff;
+			line-height: 44rpx;
+		}
+
+		.border {
+			margin-top: 80rpx;
+			width: 670rpx;
+			height: 2rpx;
+			background: rgba(255, 255, 255, 0.13);
+		}
+
+		.btn {
+			margin-top: 80rpx;
+			width: 550rpx;
+			height: 88rpx;
+			line-height: 88rpx;
+			text-align: center;
+			border: 1rpx solid #ffffff;
+			border-radius: 12rpx;
+			font-size: 32rpx;
+			color: #ffffff;
+
+		}
+	}
+
+	.remark {
+		position: absolute;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		top: 0;
+		display: flex;
+		flex-direction: column;
+		opacity: 0.94;
+		background: linear-gradient(180deg, rgba(0, 0, 0, 0.49), #000000);
+		justify-content: center;
+		align-items: center;
+
+		.flex1 {
+			flex: 1;
+		}
+
+		.remark-btn {
+			width: 396rpx;
+			height: 88rpx;
+			margin-bottom: 280rpx;
+			line-height: 88rpx;
+			text-align: center;
+			background-color: #07bfb1;
+			color: white;
+			font-size: 28rpx;
+		}
+	}
+
 	.bottom-contain {
 		position: absolute;
 		bottom: 0;
@@ -214,13 +439,17 @@
 				display: flex;
 				flex-direction: row;
 				align-items: center;
+				justify-content: center;
 				font-size: 26rpx;
 				color: #ffffff;
 				margin-left: 16rpx;
+				padding: 2rpx 8rpx;
+
 
 				.icon_macphone {
 					width: 50rpx;
 					height: 50rpx;
+					margin-right: 8rpx;
 				}
 			}
 		}
@@ -237,33 +466,70 @@
 				padding: 0 16rpx;
 				line-height: 44rpx;
 				color: #FFF;
+
 				.avater {
 					display: inline-block;
 					width: 32rpx;
 					height: 32rpx;
 					border-radius: 50%;
 				}
-				.name{
-					color: rgba( 255,255,255,0.5);
+
+				.name {
+					color: rgba(255, 255, 255, 0.5);
 				}
-				
 			}
 		}
 	}
 
 	.state-bar {
 		height: 112rpx;
-		margin: 0 24rpx;
 		display: flex;
-		justify-content: space-between;
 		align-items: center;
+
+		.user {
+			margin-left: 28rpx;
+
+			.img {
+				width: 64rpx;
+				height: 64rpx;
+				border-radius: 50%;
+			}
+
+			.name {
+				width: 100rpx;
+				height: 28rpx;
+				font-size: 20rpx;
+				color: #ffffff;
+				line-height: 28rpx;
+				text-align: center;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				white-space: nowrap;
+			}
+
+		}
+
+		.flex1 {
+			flex: 1;
+		}
+
+		.state-bar-text {
+			font-size: 28rpx;
+			font-weight: 400;
+			color: #ffffff;
+			line-height: 24rpx;
+			margin-right: 24rpx;
+		}
 	}
 
 	.content {
 		width: 100vw;
 		height: 100vh;
+		background: url('http://dbj.dragonn.top/static/mp/dabanjia/images/home/live_bg_people_full.png');
+		background-size: contain;
 		display: flex;
 		flex-direction: column;
+
 	}
 
 	.player {
