d1b55da16b927b3387cadb59298027dc6dbc747b
liys <lysd163186@163.com>
2021-10-01 16:14:22 +0800

    商品详情

--- src/sub-classify/pages/goods-detail/goods-detail.vue
+++ src/sub-classify/pages/goods-detail/goods-detail.vue
@@ -3,9 +3,8 @@
      <web-view :src="baseUrl + '/app-pages/goods-detail/index.html#wx-goodsId='+ goodId + '&wx-houseId='
         + houseId + '&wx-defaultHouseId=' + defaultHouseInfo.id  + '&wx-defaultProvinceId=' + defaultHouseInfo.provinceId
         + '&wx-defaultCityId=' + defaultHouseInfo.cityId + '&wx-defaultAreaId=' + defaultHouseInfo.areaId 
-        + '&wx-defaultLocationName=' + defaultHouseInfo.locationName + '&wx-defaultHousingEstate=' + defaultHouseInfo.housingEstate
-        + '&wx-defaultHousingEstateId=' + defaultHouseInfo.housingEstateId + '&wx-isDisabled=' + isDisabled
-        + '&wx-token=' + token + '&wx-userId=' + userId + '&wx-deviceId=' + deviceId">
+        + '&wx-defaultLocationName=' + defaultHouseInfo.name  + '&wx-isDisabled=' + isDisabled
+        + '&wx-token=' + token + '&wx-deviceId=' + deviceId">
        </web-view>
   </view>
 </template>
@@ -16,7 +15,6 @@
       return {
         baseUrl: '',
         deviceId: 0,
-        userId: 0,
         goodId: '',
         defaultHouseId: '',
         houseId: '',
@@ -26,7 +24,7 @@
       }
     },
     onLoad(e) {
-      if(e.isDisabled === '0') {
+      if(e.isDisabled === '0') {// 购物车跳转
         this.isDisabled = false
       }
       if(e.goodId){
@@ -38,7 +36,6 @@
     onShow() {
       this.token = getApp().globalData.token
       this.baseUrl = this.ENV.VUE_APP_BASE_H5
-      this.userId = uni.getStorageSync('userId')
       this.defaultHouseInfo = getApp().globalData.currentHouse
         uni.getSystemInfo({	
           success:res => {
@@ -46,11 +43,12 @@
           }
         })
         // 获取房屋信息
-        console.log(uni.getStorageSync('houseListChooseId'), "uni.getStorageSync('houseListChooseId')")
-        if(uni.getStorageSync('houseListChooseId')) {
+        if(uni.getStorageSync('houseListChooseId') !== this.defaultHouseInfo.id) {
           this.houseId = uni.getStorageSync('houseListChooseId')
         }else {
-          this.houseId = this.defaultHouseInfo.id
+          if(this.defaultHouseInfo.id) {
+            this.houseId = this.defaultHouseInfo.id
+          }
         }
     }
   }
