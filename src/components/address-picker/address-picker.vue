7f9a471a2171f23024c4dfcc615399ebee160b3c
liys <lysd163186@163.com>
2021-10-13 10:43:51 +0800

    订单

--- src/components/address-picker/address-picker.vue
+++ src/components/address-picker/address-picker.vue
@@ -8,7 +8,7 @@
          		<text class="default" v-if="addressInfo.defaultEstate">默认</text>
          		<text class="txt">{{addressInfo.housingEstate}}</text>
          	</view>
-         	<view class="community">{{addressInfo.locationName}}我的人你还打算减肥把手机放到不拿手机和电脑接口苏丹诺夫看就是能否看见撒谁看见都烦你把手机开的</view>
+         	<view class="community">{{addressInfo.locationName}}</view>
          	<view class="userInfo">
          		<text class="userName">{{addressInfo.contactName}}</text>
          		<text class="userPhone">{{addressInfo.contactPhone}}</text>
