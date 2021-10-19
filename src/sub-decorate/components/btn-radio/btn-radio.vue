9e20ed5ec7da3d1c3b03f06d93a6b8b0b1893111
wangbinyan <1159018192@qq.com>
2021-10-19 19:09:50 +0800

    fix: 更改更换设计服务列表UI样式

---
+++ src/sub-decorate/components/btn-radio/btn-radio.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="btn-radio" :class="{'checked': checked}" @click="change">
+		{{checkedStatus}}
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			checked: {
+				type: Boolean,
+				default: false,
+			},
+			value: {
+				type: String,
+				default: ""
+			}
+		},
+    computed: {
+      checkedStatus() {
+        return this.checked ? "已选" : "选择"
+      }
+    },
+		methods: {
+			change() {
+				if (!this.checked) {
+					this.$emit("change", {
+						value: this.value,
+						checked: true
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.btn-radio {
+		transition: all 0.2s;
+		box-sizing: border-box;
+    width: 100rpx;
+    height: 50rpx;
+    line-height: 50rpx;
+    font-size: 26rpx;
+    font-family: PingFangSC, PingFangSC-Regular;
+    font-weight: 400;
+    text-align: center;
+    color: #00bfb6;
+    border: 1rpx solid #00bfb6;
+    border-radius: 8rpx;
+	}
+
+	.btn-radio.checked {
+		color: #999999;
+    border-color: #cbcccc;
+	}
+</style>
