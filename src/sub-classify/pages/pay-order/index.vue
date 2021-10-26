8ab4b8c57a89ba586627fff984fe7b2acf5a0932
liys <lysd163186@163.com>
2021-10-26 17:46:26 +0800

    订单

--- src/sub-classify/pages/pay-order/index.vue
+++ src/sub-classify/pages/pay-order/index.vue
@@ -472,6 +472,7 @@
             }
           })
         })
+        uni.$emit('submitOrder') // 购物车需要的逻辑
         let params = {
           payType: 1, //"int //支付方式  1微信支付",
           openid: uni.getStorageSync("openId"), //"string //微信openid 小程序支付用 app支付不传或传空",
