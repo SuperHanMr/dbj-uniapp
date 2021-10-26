2afcd320dbf04cfbb61e04fb7cbf00442add88af
贺成龙 <mr_dragonn@foxmail.com>
2021-10-26 15:43:45 +0800

    修改首页测试

--- src/sub-my/pages/set-up/set-up.vue
+++ src/sub-my/pages/set-up/set-up.vue
@@ -1,44 +1,42 @@
 <template>
 	<view class="container">
 		<view class="body-container">
-			<view class="item-list" 
-				v-for="(item, index) in listArr" 
-				:key="item.url" 
-				@click="onClick(item,index)"
-			>
+			<view class="item-list" v-for="(item, index) in listArr" :key="item.url" @click="onClick(item,index)">
 				<view class="item">
 					<text>{{item.title}}</text>
 					<image v-if="index <(listArr.length-1)" src="../../static/icon_setup_more@2x.png" mode=""></image>
 					<text v-else>v 2.0.0</text>
 				</view>
 				<view class="line" v-if="index <(listArr.length-1)" />
-			</view>	
+			</view>
 		</view>
-		
+
 		<view class="logOut" @click="onOpen">
 			退出登录
 		</view>
-		
+
 		<uni-popup ref="popup" type="dialog" class="uni-popup">
 			<uni-popup-dialog class="uni-popup-dialog" mode="base" type="info" title="确定退出当前账号登录吗?" :duration="2000"
 				:before-close="true" @close="close" @confirm="confirm"></uni-popup-dialog>
 		</uni-popup>
-		
+
 	</view>
 </template>
 
 <script>
-	import {logout} from "../../../api/order.js"
+	import {
+		logout
+	} from "../../../api/order.js"
 	export default {
 		data() {
 			return {
 				listArr: [{
 						title: "打扮家平台服务协议",
-						url:'/static/dbj-protocol/protocol.html'
+						url: '/static/dbj-protocol/protocol.html'
 					},
 					{
 						title: "打扮家隐私政策",
-						url:'/static/dbj-protocol/privacy-policy.html'
+						url: '/static/dbj-protocol/privacy-policy.html'
 					},
 					// {
 					// 	title: "打扮家知识产权条款",
@@ -48,40 +46,40 @@
 						title: "意见与反馈",
 						url: "../../../sub-my/pages/set-up/comments-feedback/comments-feedback"
 					},
-					
+
 					{
-						title:"版本号",
-						url:""
+						title: "版本号",
+						url: ""
 					}
 				]
 			};
 		},
 		methods: {
 
-			onClick(item,index) {
-				
-				if(index <2){
-					console.log("this.ENV.VUE_APP_BASE_API=",this.ENV.VUE_APP_BASE_API)
-					
+			onClick(item, index) {
+
+				if (index < 2) {
+					console.log("this.ENV.VUE_APP_BASE_API=", this.ENV.VUE_APP_BASE_API)
+
 					let url = this.ENV.VUE_APP_BASE_API + item.url
 					uni.navigateTo({
-						url:`../../../pages/common/webview/webview?url=`+ encodeURIComponent(url),
+						url: `../../../pages/common/webview/webview?url=` + encodeURIComponent(url),
 					})
 				}
 				// if(index == 2){
 				// 	console.log("this.VUE_APP_BASE_H5=",this.ENV.VUE_APP_BASE_H5)
-					
+
 				// 	let url = this.ENV.VUE_APP_BASE_H5 + item.url
 				// 	uni.navigateTo({
 				// 		url:`../../../pages/common/webview/webview?url=`+ encodeURIComponent(url),
 				// 	})
 				// }
-				else{
+				else {
 					uni.navigateTo({
-						url:item.url
+						url: item.url
 					})
 				}
-				
+
 			},
 			onOpen() {
 				this.$refs.popup.open();
@@ -91,31 +89,36 @@
 			},
 			confirm(val) {
 				console.log("退出登录");
-				logout({clientCode:"APPLET"})
-				.then(()=>{
-					// 清除本地缓存 然后跳转到首页
-					this.$refs.popup.close();
-					uni.clearStorageSync();
-					getApp().globalData = {
-						userInfo: {},
-						token: "",
-						city: "",
-						houses: [],
-						tim: null,
-						noHouseActuaryId: null,
-						noHouseDesignId: null,
-						noHouseCheckId: null,
-						naviData: null,
-						decorateMsg: {},
-					}
-					uni.switchTab({
-						url: "../../../pages/home/index/index"
-					});
-					this.$store.dispatch("logoutIM");
-				})
-				.catch(()=>{})
+				logout({
+						clientCode: "APPLET"
+					})
+					.then(() => {
 
-				
+						// 清除本地缓存 然后跳转到首页
+						this.$refs.popup.close();
+						uni.clearStorageSync();
+						getApp().globalData = {
+							userInfo: {},
+							token: "",
+							city: "",
+							houses: [],
+							tim: null,
+							noHouseActuaryId: null,
+							noHouseDesignId: null,
+							noHouseCheckId: null,
+							naviData: null,
+							decorateMsg: {},
+						}
+						uni.$emit('logout')
+
+						uni.switchTab({
+							url: "../../../pages/home/index/index"
+						});
+						this.$store.dispatch("logoutIM");
+					})
+					.catch(() => {})
+
+
 			}
 		}
 	}
@@ -125,23 +128,26 @@
 	.container {
 		height: 100%;
 		background: #f5f6f6;
-		
-		.body-container{
+
+		.body-container {
 			background-color: #FFFFFF;
 			margin-top: 16rpx;
-			.item-list{
-				.item{
+
+			.item-list {
+				.item {
 					display: flex;
 					flex-flow: row nowrap;
 					align-items: center;
 					justify-content: space-between;
 					padding: 38rpx 24rpx 38rpx 32rpx;
-					image{
+
+					image {
 						width: 32rpx;
 						height: 34rpx;
 						object-fit: cover;
 					}
-					text{
+
+					text {
 						height: 40rpx;
 						line-height: 40rpx;
 						font-size: 28rpx;
@@ -149,7 +155,8 @@
 						color: #333333;
 					}
 				}
-				.line{
+
+				.line {
 					margin: 0 32rpx;
 					height: 1rpx;
 					background: #f2f2f2;
@@ -157,7 +164,7 @@
 				}
 			}
 		}
-		
+
 		.logOut {
 			text-align: center;
 			margin-top: 16rpx;
@@ -175,7 +182,7 @@
 		border: none;
 	}
 
- ::v-deep .uni-dialog-button-group .uni-border-left .uni-dialog-button-text {
+	::v-deep .uni-dialog-button-group .uni-border-left .uni-dialog-button-text {
 		color: rgba(255, 51, 71, 1);
 		font-size: 30rpx;
 		font-family: PingFangSC, PingFangSC-Medium;
@@ -183,7 +190,7 @@
 		text-align: left;
 	}
 
- ::v-deep .uni-dialog-title .uni-dialog-title-text {
+	::v-deep .uni-dialog-title .uni-dialog-title-text {
 		font-size: 32rpx;
 		font-family: PingFangSC, PingFangSC-Medium;
 		font-weight: 500;
