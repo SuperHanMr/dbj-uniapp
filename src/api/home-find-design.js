822f5ab4d9feaa71d841d69b7cc55af987977832
孙文龙 <1792108796@qq.com>
2022-01-17 11:05:58 +0800

    找设计、找设计列表

--- src/api/home-find-design.js
+++ src/api/home-find-design.js
@@ -1,6 +1,16 @@
 import request from '../utils/request';
 
 //更多案例列表
-export function moreCaseList(params) {
+export function moreCaseList(params = {}) {
 	return request.post(`/app/case/new/more/page`, params);
+}
+
+//设计师特色和风格
+export function designTopicStyleList(params = {}) {
+	return request.get(`/app/search/designer/params`, params);
+}
+
+//查找设计师列表
+export function designerList(params) {
+	return request.post(`/app/search/designer`, params);
 }
---
+++ src/sub-home/pages/find-design/sort-btn.vue
@@ -0,0 +1,107 @@
+<template>
+	<view class="sort">
+		<view
+			class="sort-content"
+			@click="changeSortState"
+		>
+			<text class="text">{{sort.value}}</text>
+			<view class="sort-btn">
+				<view class="sort-img up-price"></view>
+			</view>
+		</view>
+		<uni-popup ref="popup">
+			<view class="sort-type">
+				<view
+					@click="changSortType(items)"
+					class="sort-items"
+					v-for="items in sortType"
+					:key="items.key"
+				><text>{{items.value}}</text></view>
+			</view>
+		</uni-popup>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			sort: { key: 0, value: "默认排序" },
+			sortType: [
+				{ key: 0, value: "默认排序" },
+				{ key: 1, value: "服务人数" },
+				{ key: 2, value: "好评率" },
+			],
+		};
+	},
+	methods: {
+		changeSortState() {
+			this.$refs.popup.open("top");
+		},
+		changSortType(type) {
+			this.sort = type;
+			this.$refs.popup.close();
+			this.$emit("sort-type-change", type.key);
+		},
+	},
+};
+</script>
+
+<style scoped>
+.sort {
+	width: 168rpx;
+	height: 62rpx;
+	overflow: hidden;
+	background: #f9f9f9;
+	border-radius: 16rpx;
+}
+.sort-content {
+	display: flex;
+	height: 62rpx;
+	line-height: 62rpx;
+	padding: 0 16rpx;
+}
+.text {
+	font-size: 28rpx;
+	flex: 1;
+	color: #333333;
+	text-align: center;
+}
+.sort-btn {
+	width: 20rpx;
+	text-align: center;
+	position: relative;
+}
+.up-price {
+	width: 20rpx;
+	height: 10rpx;
+	background-size: cover;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	background-image: url("../../../static/images/price_up.png");
+}
+
+.sort-type {
+}
+.sort-items {
+	height: 120rpx;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	color: #222222;
+	font-weight: 500;
+	font-size: 30rpx;
+	background: #fff;
+	padding: 0 32rpx;
+}
+.sort-items:not(:last-child) {
+	border-bottom: 1px solid #f4f4f4;
+}
+.sort-items:last-child {
+	border-bottom-left-radius: 20rpx;
+	border-bottom-right-radius: 20rpx;
+}
+</style>
