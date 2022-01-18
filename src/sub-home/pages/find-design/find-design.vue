535d7acfb8eaafdd12428e0bd59ced373585628a
李书梦 <2326107425@qq.com>
2022-01-18 15:07:21 +0800

    优化设计师列表展示的问题

--- src/sub-home/pages/find-design/find-design.vue
+++ src/sub-home/pages/find-design/find-design.vue
@@ -10,85 +10,19 @@
         <view @click="toBack">
           <i
             class="icon-ic_cancel_white header-back"
-            style="color:'black'"
-          >
+            style="color:'black'">
           </i>
         </view>
       </template>
     </custom-navbar>
-
-    <!-- 	<view class="top-bg">
-			<view class="pleaholder"></view>
-		</view> -->
-    <!-- <view class="sticky-view" :style="{top:navBarHeight}">
-			<scroll-view class="tab-list" scroll-x="true" @scroll="scroll">
-				<view v-for="(item,index) in tabList" :key="item.id" class="tab-item"
-					:class="index==currentTab?'selected-tab ':'nomal-tab'" @click="onTab(index,item)">
-					<image v-if="index==0" :class="currentTab==0?'tab-img':'tab-img-un'"
-						:src=" currentTab==0? 'https://ali-image.dabanjia.com/static/mp/dabanjia/images/home/design_fire.png':'https://ali-image.dabanjia.com/static/mp/dabanjia/images/home/desigon_unfire.png'"
-						mode=""></image>
-					<text>{{item.name}}</text>
-					<view v-if="index==currentTab" class="current-select">
-
-					</view>
-				</view>
-			</scroll-view>
-
-			<scroll-view v-if="subChildren.length" style="height: 84rpx;line-height:84rpx" class="tab-list"
-				scroll-x="true" @scroll="scroll">
-				<view v-for="(item,index) in subChildren" :key="item.id" style="line-height:52rpx ;" class="tab-item"
-					:class="index==subIndex?'sub-selected':'sub-unselected'" @click="onSubTab(index,item)">
-					<text>{{item.name}}</text>
-				</view>
-			</scroll-view>
-		</view>
-		<view class="" @click="toRealCase">
-			真实案例
-		</view>
-		<design-list ref="designList"></design-list> -->
-
-    <!-- 		<view v-if="goodsList.length==0" class="noMemo">
-			<image class="noMemoImg" src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/decorate/pic_empty%402x.png">
-			</image>
-			<view class="noMemoText">暂无内容~</view>
-		</view>
-		<view class="goods-list">
-			<view class="item" v-for="(item,index) in goodsList" :key="item.id" @click="toGoodsDetail(item.id)">
-				<image class="img" :src="item.imageUrl" mode=""></image>
-				<view class="info">
-					<view class="title">
-						<text>{{item.name}}</text>
-					</view>
-					<view class="flex1">
-
-					</view>
-					<view class="price">
-						¥
-						<text class="amount">
-							{{foramtPrice(item)}}
-						</text>
-						<text class="ex">.{{formatCent(item)}}</text>
-						/{{item.unitName}}
-					</view>
-				</view>
-			</view>
-		</view>-->
-
+		
     <view
       class="header-container"
       :style="{backgroundImage:`url(${bgImg2})`}"
     >
-      <!-- <view class="backgroundStyle"
-			></view> -->
       <view class="search_container">
-        <view
-          class="search_style"
-          @click="findOwnDesigner()"
-        >
-          <image
-            src="../../static/findDesingerSearchIcon.svg"
-            mode=""
-          ></image>
+        <view class="search_style" @click="findOwnDesigner()">
+          <image src="../../static/findDesingerSearchIcon.svg" />
           <text>我想自己找...</text>
         </view>
         <image
@@ -97,19 +31,14 @@
           src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/home/online-server.png"
         />
       </view>
-      <view class="title">
-        找设计
-      </view>
+      <view class="title"> 找设计 </view>
       <view class="tab-container">
         <view
           class="tabItem"
           v-for="item1 in iconList"
           :key="item1"
         >
-          <image
-            :src="item1.url"
-            mode=""
-          ></image>
+          <image :src="item1.url" />
           <view style="color: #333333;font-size: 26rpx;">
             {{item1.name}}
           </view>
@@ -121,10 +50,7 @@
         <view class="left">
           为您推荐
         </view>
-        <view
-          class="right"
-          @click="gotoMoreDesigner"
-        >
+        <view class="right" @click="gotoMoreDesigner" >
           <text>更多</text>
           <image src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/my/small_gotoShop.svg" />
         </view>
@@ -136,8 +62,8 @@
       <scroll-view
         scroll-x="true"
         style="white-space: nowrap;"
+        @scrolltolower="gotoNext"
       >
-        <!-- @scrolltolower="gotoNext" -->
         <view
           class="design-card-item"
           v-for="item2 in searchDesignerList"
@@ -150,10 +76,7 @@
               <view class="name">{{item2.name}}</view>
               <view class="rank">{{item2.levelName}}</view>
             </view>
-            <view
-              class="goodPraise"
-              style="margin-bottom: 8rpx;"
-            >
+            <view class="goodPraise" style="margin-bottom: 8rpx;">
               <view class="item">
                 <text>好评率{{item2.praiseEfficiency}}%</text>
                 <text class="icon"></text>
@@ -165,14 +88,12 @@
             </view>
           </view>
         </view>
-        <view class="showMoreCard_container" @click="gotoMoreDesigner">
+        <view class="showMoreCard_container" >
+					<!-- @click="gotoMoreDesigner" -->
 					<view class="showMoreCard" :style="{backgroundImage:`url(${bgImg3})`,backgroundSize:'contaienr'}">
 					</view>
 					<view class="content">
-						<image
-							src="../../static/showMoreCard.svg"
-							mode=""
-						></image>
+						<image src="../../static/showMoreCard.svg"/>
 						<view class="text">
 							左滑查看更多
 						</view>
@@ -221,23 +142,23 @@
       >
         <view class="left">
           <view class="name_container">
-            <view class="name">{{item.nikeName}}</view>
+            <view class="name">{{item4.nikeName}}</view>
             <view class="cost">
-							<text v-if="item.Similarity">户型相似度{{item.Similarity}}</text>
-							<text v-if="item.Similarity" class="icon"></text>
+							<text v-if="item4.Similarity">户型相似度{{item4.Similarity}}</text>
+							<text v-if="item4.Similarity" class="icon"></text>
 							
-							<text v-if="item.flag">附近{{item.distance}}</text>
-							<text v-if="item.flag" class="icon"></text>
+							<text v-if="item4.flag">附近{{item4.distance}}</text>
+							<text v-if="item4.flag && item4.budget" class="icon"></text>
 							
-							<text v-if="!item.flag">{{item.cityName}}</text>
-							<text v-if="!item.flag" class="icon"></text>
+							<text v-if="!item4.flag">{{item4.cityName}}</text>
+							<text v-if="(!item4.flag) && item4.budget" class="icon"></text>
 							
-							<text>预算： {{item.budget || "-"}} {{item.Similarity || "-"}}</text>
+							<text>预算： {{item4.budget || "--"}}</text>
 							</view>
           </view>
           <view class="attr_container">
-            <view class="attr_item">{{item.styleName}}</view>
-            <view class="attr_item">极简简装</view>
+            <view class="attr_item" v-for="item5 in item4.features" :key="item5">{{item5}}</view>
+            <!-- <view class="attr_item">极简简装</view> -->
           </view>
         </view>
         <image
@@ -252,17 +173,8 @@
 </template>
 
 <script>
-import { findDesign, defaultSkus } from "../../../api/home.js";
-import DesignList from "./design-list.vue";
-
-import {
-  recommendCaseList,
-  searchDesigner,
-} from "../../../api/home-find-design.js";
+import { recommendCaseList, searchDesigner} from "../../../api/home-find-design.js";
 export default {
-  components: {
-    DesignList,
-  },
   data() {
     return {
       scrollTop: 0,
@@ -305,25 +217,16 @@ export default {
       userId: "",//用户id 
 			hasEstate:false,//是否有房屋
 			estateId:"",//房屋id
+			page:0,
+			totalPage:'',
     };
   },
+	
   onLoad() {
+		console.log("onLoad!!!!!!!!!!!!!")
     const systemInfo = uni.getSystemInfoSync();
     this.navBarHeight = systemInfo.statusBarHeight + "px";
-    findDesign().then((e) => {
-      this.tabList = e;
-      let id = "";
-      if (this.tabList?.length) {
-        id = this.tabList[0].id;
-      }
-      if (this.tabList?.length && this.tabList[0] && this.tabList[0].children) {
-        this.subChildren = this.tabList[0].children;
-        id = this.subChildren[0].id;
-      }
-      // this.getGoods(id)
-    });
    
-
     // 新加的
     this.userId = getApp().globalData.token;
 		// console.log("getApp().globalData.userInfo==",getApp().globalData.userInfo)
@@ -334,100 +237,103 @@ export default {
     if (this.userId) {
       // 登录
 			this.hasEstate = getApp().globalData.userInfo.hasEstate
-		
-    } else {
+		} else {
       // 未登录
 			this.estateId= ""
-    }
-		this.getDesignerList();
+		}
+		
 		this.getRecommendCaseList();
+		if(uni.getStorageSync("recommendDesignerPage")){
+			this.page = uni.getStorageSync("recommendDesignerPage")
+		}
+		// if(this.page==1){
+		// 	this.getDesignerList();
+		// }
   },
+	onShow() {
+		console.log("onShow!!!!!!!!!!!!!")
+		this.page++
+		uni.setStorageSync("recommendDesignerPage",this.page)
+		const hhh =uni.getStorageSync("recommendDesignerPage")
+		this.getDesignerList();
+		
+		// if(this.page>1){
+		// 	this.getDesignerList();
+		// }
+		console.log("this.page",this.page)
+	},
   onPageScroll(scrollTop) {
     this.scrollTop = scrollTop.scrollTop;
   },
   methods: {
-    // getGoods(id) {
-    // 	this.$refs.designList.getList(id)
-    // 	// defaultSkus({
-    // 	// 	relationType: 5,
-    // 	// 	relationId: id
-    // 	// }).then(e => {
-    // 	// 	this.goodsList = e
-    // 	// })
+   
+    // foramtPrice(item) {
+    //   let price = String(item.price || "0");
+    //   return price.slice(0, price.length - 2) || "0";
     // },
-    foramtPrice(item) {
-      let price = String(item.price || "0");
-      return price.slice(0, price.length - 2) || "0";
-    },
-    formatCent(item) {
-      let price = String(item.price || "0");
-      let fixedNum = Number(price / 100).toFixed(2);
-      if (String(fixedNum).split(".").length > 1) {
-        return String(fixedNum).split(".")[1];
-      } else {
-        return "";
-      }
-    },
-    onSubTab(index, item) {
-      this.subIndex = index;
-      // this.getGoods(item.id);
-    },
-    toGoodsDetail(id) {
-      uni.navigateTo({
-        url: "/sub-classify/pages/goods-detail/goods-detail?goodId=" + id,
-      });
-    },
-    onTab(index, item) {
-      this.currentTab = index;
-      this.subIndex = 0;
-      this.subChildren = this.tabList[index].children;
-      let id = item.id;
-      if (this.subChildren && this.subChildren.length) {
-        id = this.subChildren[0].id;
-      }
-      // this.getGoods(id);
-    },
-    toBack() {
-      uni.navigateBack({});
-    },
+    // formatCent(item) {
+    //   let price = String(item.price || "0");
+    //   let fixedNum = Number(price / 100).toFixed(2);
+    //   if (String(fixedNum).split(".").length > 1) {
+    //     return String(fixedNum).split(".")[1];
+    //   } else {
+    //     return "";
+    //   }
+    // },
+  
+    
+   
     toRealCase() {
       uni.navigateTo({
         url: "/sub-home/pages/find-design/real-case/real-case",
       });
     },
-
-    // 新加的
+		toBack() {
+		  uni.navigateBack({});
+		},
+		
     gotoNext() {
 			uni.navigateTo({
 			  url: "/sub-home/pages/find-design/search-design",
 			});
     },
-    getRecommendCaseList() {
+   
+		getRecommendCaseList() {
 			let params = {
 				estateId:this.estateId
 			}
 			console.log("params===",params)
       recommendCaseList(params).then((res) => {
         this.CaseList = res;
-        console.log("res1====", this.CaseList);
+        // console.log("res1====", this.CaseList);
       });
     },
+		
     getDesignerList() {
       let params = {
-        page: 1,
+        page: this.page,
         rows: 5,
+				// isRecommend:true,
         position: "",
         searchKey: "",
         topic: "",
         style: "",
         sortType: "", //排序类型 0:默认排序，1:服务次数排序 2： 好评率排序"
       };
+			
       searchDesigner(params).then((res) => {
+        // console.log("res2====", res);
         this.searchDesignerList = res.list;
-        // console.log("res2====", res.list);
+				this.totalPage = res.totalPage
+				uni.setStorageSync("recommendDesignerTotalPage",res.totalPage)
+				console.log("res.rows")
+				if((this.page ==(this.totalPage-1)) || res.rows % 5 !==0){
+					this.page = 0
+					uni.setStorageSync("recommendDesignerPage",this.page)
+				}
+				
       });
     },
-
     //自己找设计师
     findOwnDesigner() {
       uni.navigateTo({
@@ -499,6 +405,15 @@ export default {
     background: #ffffff;
     border-radius: 32rpx 32rpx 0rpx 0rpx;
   }
+}
+
+.header-back {
+  width: 82rpx;
+  height: 82rpx;
+  // background-color: #eee;
+  color: #000;
+  line-height: 82rpx;
+  text-align: center;
 }
 // 新添加的
 .header-container {
@@ -874,7 +789,7 @@ export default {
 					  display: block;
 					  height: 20rpx;
 					  width: 2rpx;
-					  background: #ffffff;
+					  background: #999999;
 					  opacity: 0.7;
 					  margin: 0 16rpx;
 					}
@@ -904,211 +819,4 @@ export default {
   }
 }
 
-// 之前的
-// page {
-// 	background: #FFF;
-// }
-.header-back {
-  width: 82rpx;
-  height: 82rpx;
-  // background-color: #eee;
-  color: #000;
-  line-height: 82rpx;
-  text-align: center;
-}
-.tes1t {
-  width: 100%;
-  height: 10000rpx;
-  background-color: yellow;
-}
-
-.noMemo {
-  width: 100%;
-  height: fit-content;
-}
-
-.noMemo .noMemoImg {
-  width: 750rpx;
-  height: 580rpx;
-  display: block;
-}
-
-.noMemo .noMemoText {
-  width: fit-content;
-  height: 36rpx;
-  margin-top: 24rpx;
-  margin-left: 324rpx;
-  font-size: 26rpx;
-  color: #999999;
-}
-
-.sub-selected {
-  background: linear-gradient(129deg, #00cdec 4%, #00ed7d 100%);
-  border-radius: 12rpx;
-  font-size: 24rpx;
-  padding: 16rpx;
-  font-weight: 500;
-  color: #ffffff;
-  margin-left: 20rpx;
-}
-
-.sub-unselected {
-  background: #f7f7f7;
-  color: #999999;
-  border-radius: 12rpx;
-  font-size: 24rpx;
-  padding: 16rpx;
-  font-weight: 400;
-  margin-left: 20rpx;
-}
-
-.tab-item {
-  padding: 0 20rpx;
-  text-align: center;
-  display: inline-block;
-  position: relative;
-}
-
-.current-select {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 32rpx;
-  height: 4rpx;
-  background: linear-gradient(129deg, #00cdec 0%, #00ed7d 92%);
-  border-radius: 200rpx 200rpx 0rpx 0rpx;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.tab-img {
-  display: inline-block;
-  width: 32rpx;
-  height: 32rpx;
-}
-
-.tab-img-un {
-  display: inline-block;
-  width: 26rpx;
-  height: 26rpx;
-}
-
-.nomal-tab {
-  font-size: 26rpx;
-  font-weight: 400;
-  color: #696d70;
-}
-
-.selected-tab {
-  font-weight: 500;
-  font-size: 32rpx;
-  color: #2d3033;
-}
-
-.sticky-view {
-  position: sticky;
-  position: -webkit-sticky;
-  left: 0;
-  right: 0;
-  background-color: white;
-}
-
-.tab-list {
-  height: 64rpx;
-  line-height: 64rpx;
-  width: 100%;
-  overflow: hidden;
-  white-space: nowrap;
-}
-
-.goods-list {
-  display: flex;
-  margin-top: 24rpx;
-  width: 100%;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-
-  .item {
-    margin-top: 16rpx;
-    flex-shrink: 0;
-    width: 343rpx;
-    height: 520rpx;
-    border-radius: 18rpx;
-    border: 1rpx solid #e6eaed;
-    margin-left: 21.3rpx;
-    position: relative;
-    overflow: hidden;
-
-    .img {
-      width: 100%;
-      height: 343rpx;
-    }
-
-    .price {
-      margin-bottom: 28rpx;
-      font-size: 24rpx;
-      font-weight: 400;
-      color: #939699;
-      line-height: 26rpx;
-
-      .amount {
-        font-size: 40rpx;
-        font-weight: 400;
-        color: #2b2f33;
-        line-height: 42rpx;
-      }
-
-      .ex {
-        font-size: 24rpx;
-        font-weight: 400;
-        color: #2b2f33;
-        line-height: 26rpx;
-      }
-    }
-
-    .info {
-      padding: 0 24rpx;
-      display: flex;
-      flex-direction: column;
-      height: 177rpx;
-
-      .title {
-        margin-top: 10rpx;
-        font-size: 28rpx;
-        font-weight: 600;
-        color: #2b2f33;
-        line-height: 40rpx;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2; //这个代表你要在几行显示省略号
-        -webkit-box-orient: vertical;
-      }
-
-      .tip {
-        display: inline-block;
-        height: 30rpx;
-        line-height: 30rpx;
-        margin-right: 10rpx;
-        width: 60rpx;
-        border-radius: 4rpx;
-        border: 1rpx solid #00bfb6;
-        text-align: center;
-        font-weight: 500;
-        color: #00bfb6;
-        font-size: 20rpx;
-      }
-
-      .tip:before,
-      .tip:after {
-        display: inline-block;
-        vertical-align: middle;
-        content: "";
-        height: 100%;
-      }
-    }
-  }
-}
 </style>
