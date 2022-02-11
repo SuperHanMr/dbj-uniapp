233c3acf36dbccacfe10dc3cea3e5651c003556f
liys <lysd163186@163.com>
2022-02-11 11:36:57 +0800

    支付

--- src/sub-classify/pages/pay-order/pay-h5.vue
+++ src/sub-classify/pages/pay-order/pay-h5.vue
@@ -14,6 +14,7 @@
         <view class="choose-pay"></view>
       </view>
     </view>
+    <view @click="toApp">测试跳转</view>
     <view class="pay-btn price-font" @click="pay">
       微信支付 ¥ {{(totalPrice/100).toFixed(2)}}
     </view>
@@ -68,6 +69,9 @@
       // })
     },
     methods: {
+      toApp() {
+        location.href = 'gome://m.gome.com.cn/appHome'
+      },
       pay() {
         let params = {
           payTal: this.payTal
