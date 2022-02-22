7f4c681b303ff73eeb0f21f0026df0797ebf8bee
李书梦 <2326107425@qq.com>
2022-02-22 18:06:03 +0800

    添加方法的跳转路径

--- src/sub-home/pages/find-design/designer-rank-list.vue
+++ src/sub-home/pages/find-design/designer-rank-list.vue
@@ -22,18 +22,19 @@
 		</view>
 		<view class="list-container">
 			<view class="designer-item" v-for="(item1,index1) in designerList" :key="index1">
-				<view class="top-bg" 
+				<view class="designer-bg"
 					:style="{backgroundColor:index1>2?bgColorList[3].bgColor:bgColorList[index1].bgColor}"
 				></view>
-				<view class="topNum" 
-				:style="{backgroundImage:index1>2?`url(${bgColorList[3].bgImg})`:`url(${bgColorList[index1].bgImg})`,backgroundSize:'100% 100%'}">
-					<view class="top-font"
-					:style="{color:index1>2?bgColorList[3].color:bgColorList[index1].color}"
-					>{{index1+1}}</view>
-				</view>
-				<view class="info-container">
-					<view class="header">
-						<image class="avatar" @click="gotoPersonalPage(item1)"  :src="`${item1.searchDesignerVO.avatar}?x-oss-process=image/resize,m_fill,h_88,w_88,limit_0`" />
+				<view class="basicInfo-container">
+					<view class="designer-topNum"
+					:style="{backgroundImage:index1>2?`url(${bgColorList[3].bgImg})`:`url(${bgColorList[index1].bgImg})`,backgroundSize:'100% 100%'}">
+						<view class="top-font"
+						:style="{color:index1>2?bgColorList[3].color:bgColorList[index1].color}"
+						>{{index1+1}}</view>
+					</view>
+
+					<view class="designer-info">
+						<image class="avatar" @click="gotoPersonalPage(item1,'part')"  :src="`${item1.searchDesignerVO.avatar}?x-oss-process=image/resize,m_fill,h_88,w_88,limit_0`" />
 						<view class="basic-info">
 							<view class="name-container">
 								<text class="name"> {{item1.searchDesignerVO.name}}</text>
@@ -41,33 +42,34 @@
 							</view>
 							<view class="attr">
 								<!-- liveArea 居住地 -->
-								<view class="liveAreaValue" v-if="item1.liveArea" 
+								<view class="liveAreaValue" v-if="item1.liveArea"
 									:class="{anotherStyle:item1.liveArea.length>=8}"
 								><text>{{item1.liveArea}}</text></view>
 								<view class="attrValue">{{item1.liveArea?"&nbsp;| ":''}}服务次数{{item1.searchDesignerVO.totalPeopleCount || "0" }}  |  好评率{{item1.searchDesignerVO.praiseEfficiency || "0"}}%</view>
 							</view>
 							<view class="label_container">
-							<view 
-								class="label_item" v-if="item1.searchDesignerVO.designs.length>0" v-for="tagItem in item1.searchDesignerVO.designs" :key="tagItem">{{tagItem}}</view>
+							<view
+								class="label_item" v-if="item1.searchDesignerVO.designs.length" v-for="tagItem in item1.searchDesignerVO.designs" :key="tagItem">{{tagItem}}</view>
 							</view>
 						</view>
 						<view class="hasAttention" v-if="item1.isFocusOn" @click.stop="handleDesigner(item1,index1)">
 							已关注
 						</view>
-						<view class="attention" v-else @click.stp="handleDesigner(item1,index1)">
+						<view class="attention" v-else @click.stop="handleDesigner(item1,index1)">
 							<image src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/home/attentionIcon.png"  />
 							<text>关注</text>
 						</view>
 					</view>
+
 					<view class="case-container">
 						<scroll-view
 							scroll-x="true"
 							style="white-space: nowrap;"
-							@scrolltolower.stop="gotoPersonalPage(item1)"
+							@scrolltolower.stop="gotoPersonalPage(item1,'total')"
 							:scroll-left="scrollLeft"
 						>
 						<view class="case-content">
-							<view class="case-item" v-for="(item2,index2) in item1.valuationCaseVOS" :key="item2.id" @click="gotoCaseDetail">
+							<view class="case-item" v-for="item2 in item1.valuationCaseVOS" :key="item2.id" @click="gotoCaseDetail(item2)">
 								<image v-if="item2.famous" class="icon" src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/home/famousWork.png" />
 								<image v-if="item2.favourite" class="icon" src="https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/home/favouriteWork.png" />
 
@@ -88,12 +90,12 @@
 										<text>{{item2.hallNum || "-"}}厅</text>
 									</text>
 									<text class="line" v-if="item2.roomNum || item2.hallNum"></text>
-									
+
 									<text class="text">{{item2.insideArea?`${Math.floor(item2.insideArea)}m²`: "-"}}</text>
 									<text class="line"></text>
-									
+
 									<text class="text">预算：{{ item2.budget?`${Math.floor(item2.budget)}万` : '-'}}</text>
-								</view> 
+								</view>
 							</view>
 							<view class="show-more" v-if="item1.valuationCaseVOS.length >2">
 								<view class="text">左滑查看更多</view>
@@ -101,6 +103,7 @@
 						</view>
 						</scroll-view>
 					</view>
+
 				</view>
 			</view>
 		</view>
@@ -199,15 +202,18 @@
 			toBack(){
 				uni.navigateBack({});
 			},
-			gotoPersonalPage(item){
-				console.log("去设计师主页",item)
-				if(item.valuationCaseVOS.length<=2) return 
-				uni.navigateTo({ 
+			gotoPersonalPage(item, type){
+				if(type =="total" && item.valuationCaseVOS.length<=2) return ;
+				uni.navigateTo({
 					url:`../../../sub-decorate/pages/person-page/person-page?personId=${item.searchDesignerVO.id}&isToContent=true`
 				})
 			},
-			gotoCaseDetail(){
-				console.log("去案例详情")
+			gotoCaseDetail(item){
+				console.log("去案例详情",item)
+				uni.navigateTo({
+					url: `/pages/real-case/real-case-webview/real-case-webview?id=${item.id}`
+				})
+
 			},
 			handleDesigner(item,index){
 				console.log("是否关注设计师")
@@ -237,7 +243,7 @@
 					}
 				})
 			},
-			
+
 			openExplain(){
 				this.$refs.explainPopup.open()
 			},
@@ -279,20 +285,18 @@
 				margin-bottom: 10rpx;
 			}
 		}
-		
+
 	}
 	.list-container{
 		box-sizing: border-box;
 		padding: 400rpx 24rpx 24rpx;
 		height: 100%;
 		.designer-item{
-			width: 702rpx;
-			height: 624rpx;
 			padding-top: 40rpx;
 			box-sizing: border-box;
 			position: relative;
 			margin-bottom: 24rpx;
-			.top-bg{
+			.designer-bg{
 				position: absolute;
 				top: 24rpx;
 				left: 0;
@@ -301,281 +305,292 @@
 				border-radius: 24rpx 24rpx 0 0;
 				background: #EDC48E;
 			}
-			.topNum{
-				position: absolute;
-				left: 32rpx;
-				top: 0;
-				width: 74rpx;
-				height: 82rpx;
-				z-index: 3;
-				word-wrap: break-word;
-				display: flex;
-				justify-content: space-around;
-				view{
-					margin-top: 32rpx;
-					font-size: 32rpx;
-					font-weight: bold;
-					height: 44rpx;
-					line-height: 44rpx;
-				}
-			}
-			.info-container{
-				position: absolute;
-				width: 702rpx;
+			.basicInfo-container{
 				background-color: #FFFFFF;
 				border-radius: 24rpx;
 				z-index: 2;
-				.header{
+				position: relative;
+				.designer-topNum{
+					position: absolute;
+					left: 32rpx;
+					top: -46rpx;
+					width: 74rpx;
+					height: 82rpx;
+					z-index: 3;
+					word-wrap: break-word;
 					display: flex;
-					flex-flow: row nowrap;
-					justify-content: flex-start;
-					padding: 46rpx 24rpx 20rpx;
-					position: relative;
-					.avatar{
-						width: 88rpx;
-						height: 88rpx;
-						border-radius: 50%;
-						margin-right: 16rpx;
-					}
-					.basic-info{
-						.name-container{
-							display: flex;
-							flex-flow: row nowrap;
-							align-items: center;
-							margin-bottom: 8rpx;
-							.name{
-								display: block;
-								height: 44rpx;
-								line-height: 44rpx;
-								color: #222222;
-								font-size: 32rpx;
-								font-weight: 500;
-								margin-right: 24rpx;
-							}
-							.rank{
-								display: block;
-								height: 30rpx;
-								padding: 0 8rpx 2rpx;
-								box-sizing: border-box;
-								line-height: 30rpx;
-								background: rgba(79, 190, 237, 0.06);
-								border-radius: 4rpx;
-								text-align: center;
-								color: #4FBEED;
-								font-size: 20rpx;
-
-							}
-						}
-						.attr {
-							max-width: 416rpx;
-							height: 30rpx;
-							margin-bottom: 16rpx;
-							display: flex;
-							flex-flow: row nowrap;
-							align-items: center;
-							flex: 1;
-							.liveAreaValue{
-								display: flex;
-								// flex: 1;
-								overflow: hidden;
-								text{
-									overflow: hidden;
-									white-space: nowrap; 
-									text-overflow: ellipsis;
-									height: 30rpx;
-									line-height: 30rpx;
-									color: #333333;
-									font-size: 22rpx;
-								}
-							}
-							
-							.attrValue{
-								height: 30rpx;
-								line-height: 30rpx;
-								color: #333333;
-								font-size: 22rpx;
-								
-							}
-						}
-						
-						.label_container {
-							display: flex;
-							flex-flow: row wrap;
-							width: 536rpx;
-							max-height: 104rpx;
-							overflow: hidden;
-							flex: 1;
-							align-items: center;
-							.label_item {
-								height: 32rpx;
-								line-height: 32rpx;
-								padding: 4rpx 12rpx 6rpx;
-								border: 1rpx solid #E8E8E8;
-								background: #FFFFFF;
-								border-radius: 6rpx;
-								margin-right: 12rpx;
-								margin-bottom: 12rpx;
-								color: #999999;
-								text-align: center;
-								font-size: 24rpx;
-							}
-						}
-					}
-					.attention{
-						position: absolute;
-						width: 126rpx;
-						height: 56rpx;
-						background: linear-gradient(116.19deg, #F83112 16.48%, #FD6421 83.52%);
-						border-radius: 8rpx;
-						display: flex;
-						align-items: center;
-						flex-flow: row nowrap;
-						padding: 0 24rpx;
-						box-sizing: border-box;
-						top: 46rpx;
-						right: 24rpx;
-						image{
-							width: 20rpx;
-							height: 20rpx;
-							margin-right: 10rpx;
-						}
-						text{
-							font-size: 24rpx;
-							line-height: 36rpx;
-							color: #FFFFFF;
-							font-weight: 500;
-						}
-					}
-					.hasAttention{
-						position: absolute;
-						width: 126rpx;
-						height: 56rpx;
-						line-height: 56rpx;
-						text-align: center;
-						background: #F3F3F3;
-						border-radius: 8rpx;
-						color: #666666;
-						font-size: 24rpx;
-						font-weight: 500;
-						top: 46rpx;
-						right: 24rpx;
+					justify-content: space-around;
+					view{
+						margin-top: 32rpx;
+						font-size: 32rpx;
+						font-weight: bold;
+						height: 44rpx;
+						line-height: 44rpx;
 					}
 				}
-				.case-container{
-					padding-left: 24rpx;
-					padding-bottom: 32rpx;
-					scroll-view{
-						.case-content{
+			}
+
+		}
+
+	}
+	.designer-info{
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: flex-start;
+		padding: 46rpx 24rpx 20rpx;
+		position: relative;
+		.avatar{
+			width: 88rpx;
+			height: 88rpx;
+			border-radius: 50%;
+			margin-right: 16rpx;
+		}
+		.basic-info{
+			.name-container{
+				display: flex;
+				flex-flow: row nowrap;
+				align-items: center;
+				margin-bottom: 8rpx;
+				.name{
+					display: block;
+					height: 44rpx;
+					line-height: 44rpx;
+					color: #222222;
+					font-size: 32rpx;
+					font-weight: 500;
+					margin-right: 24rpx;
+				}
+				.rank{
+					display: block;
+					height: 30rpx;
+					padding: 0 8rpx 2rpx;
+					box-sizing: border-box;
+					line-height: 30rpx;
+					background: rgba(79, 190, 237, 0.06);
+					border-radius: 4rpx;
+					text-align: center;
+					color: #4FBEED;
+					font-size: 20rpx;
+
+				}
+			}
+			.attr {
+				max-width: 416rpx;
+				height: 30rpx;
+				margin-bottom: 16rpx;
+				display: flex;
+				flex-flow: row nowrap;
+				align-items: center;
+				flex: 1;
+				.liveAreaValue{
+					display: flex;
+					// flex: 1;
+					overflow: hidden;
+					text{
+						overflow: hidden;
+						white-space: nowrap;
+						text-overflow: ellipsis;
+						height: 30rpx;
+						line-height: 30rpx;
+						color: #333333;
+						font-size: 22rpx;
+					}
+				}
+
+				.attrValue{
+					height: 30rpx;
+					line-height: 30rpx;
+					color: #333333;
+					font-size: 22rpx;
+
+				}
+			}
+
+			.label_container {
+				display: flex;
+				flex-flow: row wrap;
+				width: 536rpx;
+				max-height: 104rpx;
+				overflow: hidden;
+				flex: 1;
+				align-items: center;
+				.label_item {
+					height: 32rpx;
+					line-height: 32rpx;
+					padding: 4rpx 12rpx 6rpx;
+					border: 1rpx solid #E8E8E8;
+					background: #FFFFFF;
+					border-radius: 6rpx;
+					margin-right: 12rpx;
+					margin-bottom: 12rpx;
+					color: #999999;
+					text-align: center;
+					font-size: 24rpx;
+				}
+			}
+		}
+		.attention{
+			position: absolute;
+			width: 126rpx;
+			height: 56rpx;
+			background: linear-gradient(116.19deg, #F83112 16.48%, #FD6421 83.52%);
+			border-radius: 8rpx;
+			display: flex;
+			align-items: center;
+			flex-flow: row nowrap;
+			padding: 0 24rpx;
+			box-sizing: border-box;
+			top: 46rpx;
+			right: 24rpx;
+			image{
+				width: 20rpx;
+				height: 20rpx;
+				margin-right: 10rpx;
+			}
+			text{
+				font-size: 24rpx;
+				line-height: 36rpx;
+				color: #FFFFFF;
+				font-weight: 500;
+			}
+		}
+		.hasAttention{
+			position: absolute;
+			width: 126rpx;
+			height: 56rpx;
+			line-height: 56rpx;
+			text-align: center;
+			background: #F3F3F3;
+			border-radius: 8rpx;
+			color: #666666;
+			font-size: 24rpx;
+			font-weight: 500;
+			top: 46rpx;
+			right: 24rpx;
+		}
+	}
+
+	.case-container{
+		padding-left: 24rpx;
+		padding-bottom: 32rpx;
+		scroll-view{
+			.case-content{
+				display: flex;
+				align-items: center;
+				flex-flow: row nowrap;
+				.case-item-style{
+					position: relative;
+					display: inline-block;
+					margin-right: 24rpx;
+
+				}
+				.case-item{
+					position: relative;
+					width: 462rpx;
+					height: 336rpx;
+					display: inline-block;
+					margin-right: 24rpx;
+					.icon{
+						position: absolute;
+						width: 132rpx;
+						height: 42rpx;
+						top: 16rpx;
+						left: 16rpx;
+					}
+					.img-Container{
+						height: 242rpx;
+						margin-bottom: 20rpx;
+						border-radius: 12rpx;
+						.oneImg{
+							width: 462rpx;
+							height: 242rpx;
+							border-radius: 12rpx;
+						}
+						.threeImg{
 							display: flex;
 							align-items: center;
 							flex-flow: row nowrap;
-							.case-item{
-								position: relative;
-								width: 462rpx;
-								height: 336rpx;
-								display: inline-block;
-								margin-right: 24rpx;
-								.icon{
-									position: absolute;
-									width: 132rpx;
-									height: 42rpx;
-									top: 16rpx;
-									left: 16rpx;
-								}
-								.img-Container{
-									height: 242rpx;
-									margin-bottom: 20rpx;
-									border-radius: 12rpx;
-									.oneImg{
-										width: 462rpx;
-										height: 242rpx;
-										border-radius: 12rpx;
-									}
-									.threeImg{
-										display: flex;
-										align-items: center;
-										flex-flow: row nowrap;
-										.bigImg{
-											width: 308rpx;
-											height: 242rpx;
-											margin-right: 4rpx;
-											border-radius: 12rpx 0 0 12rpx;
-										}
-										.smallImg-Container{
-											display: flex;
-											flex-flow: column nowrap;
-											.smalImg1{
-												width: 150rpx;
-												height: 120rpx;
-												margin-bottom: 4rpx;
-												border-radius: 0 12rpx 0 0;
-											}
-											.smalImg2{
-												width: 150rpx;
-												height: 120rpx;
-												border-radius: 0 0 12rpx 0;
-											}
-										}
-									}
-								}
-								.caseName{
-									width: 450rpx;
-									height: 36rpx;
-									line-height: 36rpx;
-									color: #333333;
-									font-size: 26rpx;
-									letter-spacing: 1rpx;
-									margin-bottom: 8rpx;
-									overflow: hidden;
-									white-space: nowrap;
-									text-overflow: ellipsis;
-								}
-								.caseInfo{
-									height: 30rpx;
-									font-size: 22rpx;
-									margin-bottom: 16rpx;
-									display: flex;
-									flex-flow: row nowrap;
-									align-items: center;
-									text {
-										line-height: 30rpx;
-										color: #999999;
-									}
-									.line {
-										display: block;
-										height: 20rpx;
-										width: 2rpx;
-										background: #999999;
-										opacity: 0.7;
-										margin: 0 16rpx;
-									}
-								}
+							.bigImg{
+								width: 308rpx;
+								height: 242rpx;
+								margin-right: 4rpx;
+								border-radius: 12rpx 0 0 12rpx;
 							}
-							.show-more{
-								display: inline-block;
-								width: 64rpx;
-								height: 338rpx;
-								.text{
-									width: 64rpx;
-									height: 242rpx;
-									font-size: 24rpx;
-									text-align: center;
-									color: #666666;
-									letter-spacing: 8rpx;
-									padding: 26rpx 20rpx 24rpx;
-									box-sizing: border-box;
-									background-color: #F3F3F3;
-									border-radius: 12rpx 0 0 12rpx;
-									writing-mode: vertical-lr;/*从左向右 从右向左是 writing-mode: vertical-rl;*/
-									writing-mode: tb-lr;/*IE浏览器的从左向右 从右向左是 writing-mode: tb-rl；*/
+							.smallImg-Container{
+								display: flex;
+								flex-flow: column nowrap;
+								.smalImg1{
+									width: 150rpx;
+									height: 120rpx;
+									margin-bottom: 4rpx;
+									border-radius: 0 12rpx 0 0;
+								}
+								.smalImg2{
+									width: 150rpx;
+									height: 120rpx;
+									border-radius: 0 0 12rpx 0;
 								}
 							}
 						}
+					}
+					.caseName{
+						width: 450rpx;
+						height: 36rpx;
+						line-height: 36rpx;
+						color: #333333;
+						font-size: 26rpx;
+						letter-spacing: 1rpx;
+						margin-bottom: 8rpx;
+						overflow: hidden;
+						white-space: nowrap;
+						text-overflow: ellipsis;
+					}
+					.caseInfo{
+						height: 30rpx;
+						font-size: 22rpx;
+						margin-bottom: 16rpx;
+						display: flex;
+						flex-flow: row nowrap;
+						align-items: center;
+						text {
+							line-height: 30rpx;
+							color: #999999;
+						}
+						.line {
+							display: block;
+							height: 20rpx;
+							width: 2rpx;
+							background: #999999;
+							opacity: 0.7;
+							margin: 0 16rpx;
+						}
+					}
+				}
+
+				.show-more{
+					display: inline-block;
+					width: 64rpx;
+					height: 338rpx;
+					.text{
+						width: 64rpx;
+						height: 242rpx;
+						font-size: 24rpx;
+						text-align: center;
+						color: #666666;
+						letter-spacing: 8rpx;
+						padding: 26rpx 20rpx 24rpx;
+						box-sizing: border-box;
+						background-color: #F3F3F3;
+						border-radius: 12rpx 0 0 12rpx;
+						writing-mode: vertical-lr;/*从左向右 从右向左是 writing-mode: vertical-rl;*/
+						writing-mode: tb-lr;/*IE浏览器的从左向右 从右向左是 writing-mode: tb-rl；*/
 					}
 				}
 			}
 		}
 	}
+
+
 	.header-popup {
 		width: 750rpx;
 		height: 102rpx;
@@ -629,5 +644,9 @@
 	.anotherStyle{
 		flex: 1;
 	}
+
+
+
+
 
 </style>
