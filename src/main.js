e09b42fee3d21eb084bce59d32a039534e0eb85d
zhaowenhao <wenhao880204@163.com>
2022-03-02 16:49:51 +0800

    chore: H5页面中移除token

--- src/main.js
+++ src/main.js
@@ -66,25 +66,29 @@ let params = (function (a) {
   return ret;
 })(window.location);
 function removeUrlToken() {
+  // 在APP中时不移除url中的token，因为在ios版本的真快乐APP移除token之后，登录状态失效
+  if (window.GomeJSBridge && window.GomeJSBridge.ready) {
+    return;
+  }
   window.location.replace('/' + (params.isGomeMp ? '?isGomeMp=true' : '') + window.location.hash);
 }
 if (params.token) {
   if (params.token === "CLEAR") {
     uni.clearStorageSync("scn");
     uni.clearStorageSync("userId");
-    // removeUrlToken();
+    removeUrlToken();
   } else {
     uni.setStorage({
       key: "scn",
       data: params.token,
       success: function() {
         console.log("save scn success");
-        // removeUrlToken();
+        removeUrlToken();
       },
       fail: function(e) {
         console.error("save scn fail!", e);
         localStorage.setItem("scn", params.token);
-        // removeUrlToken();
+        removeUrlToken();
       }
     });
   }
