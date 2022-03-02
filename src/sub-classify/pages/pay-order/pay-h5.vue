5839e15c46243cedc5fedafc9dd217f735fce53e
liys <lysd163186@163.com>
2022-03-02 12:38:20 +0800

    支付

--- src/sub-classify/pages/pay-order/pay-h5.vue
+++ src/sub-classify/pages/pay-order/pay-h5.vue
@@ -74,8 +74,8 @@
           payTal: this.payTal
         }
         payH5(params).then((data) => {
-          let payUrl = data.url + encodeURIComponent(location.href + '&isRedirect=1')
-          // let payUrl = data.url + 'gome://m.gome.com.cn/appHome_2.html'
+          // let payUrl = data.url + encodeURIComponent(location.href + '&isRedirect=1')
+          let payUrl = data.url + encodeURIComponent('gome://m.gome.com.cn/appHome_2.html')
           location.href = payUrl
         })
       },
