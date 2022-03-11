81a3fb6c967d54bbd3bb046d0d699d6303ea77e8
李书梦 <2326107425@qq.com>
2022-03-11 15:38:02 +0800

    商城需求开发

---
+++ src/sub-classify/pages/rank-list/rank-list.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="contaienr">
+		<custom-navbar
+		  :opacity="scrollTop/100"
+		  title="品牌馆"
+		  bgcolor=""
+		>
+		  <template v-slot:back>
+		    <view @click="toBack">
+		      <i
+		        class="icon-ic_cancel_white header-back"
+		        style="color:white"
+		      >
+		      </i>
+		    </view>
+		  </template>
+		</custom-navbar>
+		<view class="bgImg" :style="{backgroundImage:`url(${headerBgImg})`}">
+		</view>
+		
+		<view class="rankList-container">
+			<view class="rankItem" v-for="item in 20" >
+				<image class="rankImage" src="../../../static/images/no-goods.png" mode=""></image>
+				<view class="rankName">索菲亚家具</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				headerBgImg:"https://ali-image.dabanjia.com/static/mp/dabanjia/images/theme-red/home/designerRankBg.png?x-oss-process=image/resize,m_fill,h_382,w_750,limit_0",
+			}
+		},
+		onLoad() {
+			this.reqRankList()
+		},
+		methods: {
+			toBack(){
+				uni.navigateBack({})
+			},
+			reqRankList(){
+				
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+	.container{
+		height: 100%;
+		background: #fff;
+		position: relative;
+	}
+	.bgImg{
+		height: 382rpx;
+		width: 750rpx;
+		background-size: contain;
+		position: fixed;
+		z-index: 8;
+		top: 0;
+		left: 0;
+	}
+	.rankList-container{
+		display: flex;
+		flex-flow: row wrap;
+		box-sizing: border-box;
+		padding: 400rpx 0 24rpx 40rpx;
+		height: 100%;
+		background: linear-gradient(180.26deg, #0F1211 4.86%, rgba(24, 40, 77, 0.72) 64.62%, #101212 99.77%);
+		.rankItem{
+			width: 320rpx;
+			height: 238rpx;
+			margin-bottom: 52rpx;
+			background: linear-gradient(180deg, rgba(29, 50, 95, 0) 48.23%, rgba(57, 142, 221, 0.1) 100.11%);
+			box-shadow: 0rpx 2rpx 8rpx rgba(3, 11, 32, 0.31);
+			border-radius: 24rpx;
+			display: flex;
+			align-items: center;
+			flex-flow: column nowrap;
+			margin-right: 30rpx;
+			.rankImage{
+				width: 84rpx;
+				height: 84rpx;
+				border-radius: 50%;
+				margin: 84rpx auto 14rpx;
+			}
+			.rankName{
+				height: 36rpx;
+				line-height: 36rpx;
+				font-size: 26rpx;
+				letter-spacing: 1rpx;
+				color: #ffffff;
+			}
+		}
+	}
+		
+</style>
---
+++ src/sub-classify/pages/shop-store-list/shop-store-list.vue
@@ -0,0 +1,154 @@
+<template>
+	<view class="container"> 
+		<view class="store-container">
+			<view class="storeItem" v-for="item in 4" :key="item">
+				<image class="storeImg" src="../../static/image/pay-bg.png" mode=""></image>
+				<view class="baseInfo-container">
+					<view class="header">
+						<image class="icon" src="../../../static/home_owner.png" mode=""></image>
+						<view class="title">芝华仕家具(北京石景山区店)</view>
+					</view>
+					<view class="label-container">
+						<view class="labelItem">家具</view>
+						<view class="labelItem">全屋定制</view>
+						<view class="labelItem">软装</view>
+					</view>
+					<view class="product-container">
+						<view class="productItem" v-for="item2 in 3" :key="item2">
+							<image class="productImg" src="../../../static/images/real-case/pic_empty.png" mode=""></image>
+							<view class="productName">简约柜子绿色...</view>
+							<view class="price">
+								<text style="font-size: 22rpx;">￥</text>
+								<text style="font-size: 28rpx;font-weight: 500;">88.88</text>
+								<text style="font-size: 20rpx;">/个</text>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				trigger:false,
+				onLoading:false,
+				storeList:[],
+			}
+		},
+		onLoad() {
+			this.reqStoreList()
+		},
+		methods: {
+			reqStoreList(){
+				
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+	.container{
+		background: #F5F5F5;
+		.store-container{
+			height: 100%;
+			overflow: scroll;
+			
+		}
+	}	
+	.storeItem{
+		margin-top: 16rpx;
+		height: 448rpx;
+		background: #ffffff;
+		border-radius: 24rpx;
+		display: flex;
+		justify-content: flex-start;
+		padding: 24rpx 0 0 24rpx;
+		.storeImg{
+			width: 108rpx;
+			height: 108rpx;
+			border: 1rpx solid #F2F2F2;
+			box-sizing: border-box;
+			border-radius: 50%;
+			margin-right: 20rpx;
+		}
+		.baseInfo-container{
+			.header,.label-container,.product-container{
+				display: flex;
+				flex-flow: row nowrap;
+				align-items: center;
+			}
+			
+			.header{
+				margin-bottom: 12rpx;
+				.icon{
+					width: 50rpx;
+					height: 28rpx;
+					border-radius: 4rpx;
+					margin-right: 10rpx;
+				}
+				.title{
+					max-width: 506rpx;
+					height: 44rpx;
+					line-height: 44rpx;
+					color: #333333;
+					font-size: 32rpx;
+					font-weight: bold;
+					letter-spacing: 1rpx;
+					overflow: hidden;
+					white-space: nowrap;
+					text-overflow: ellipsis;
+				}
+			} 
+			
+			.label-container{
+				margin-bottom: 28rpx;
+				.labelItem{
+					padding: 0 12rpx;
+					height: 36rpx;
+					line-height: 36rpx;
+					color: #586E85;
+					font-size: 22rpx;
+					background: #F2F4F7;
+					border-radius: 6rpx;
+					margin-right: 16rpx;
+				}
+			}
+			
+			.product-container{
+				.productItem{
+					display: flex;
+					flex-flow: column nowrap;
+					margin-right: 16rpx;
+					.productImg{
+						width: 178rpx;
+						height: 178rpx;
+						margin-bottom: 16rpx;
+					}
+					.productName{
+						height: 40rpx;
+						line-height: 40rpx;
+						font-size: 24rpx;
+						letter-spacing: 1rpx;
+						color: #333333;
+						overflow: hidden;
+						white-space: nowrap;
+						text-overflow: ellipsis; 
+						margin-bottom: 4rpx;
+					}
+					
+					.price{
+						text{
+							color: #333333;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+</style>
