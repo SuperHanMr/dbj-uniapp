27b48b049c99e566cbb392bc1eb9589ff0255ac4
HanYongHui <xd66620147@163.com>
2022-03-24 16:28:39 +0800

    案例详情 修改地址

--- src/pages/real-case/real-case-webview/real-case-webview.vue
+++ src/pages/real-case/real-case-webview/real-case-webview.vue
@@ -40,8 +40,7 @@
     },
 
     onLoad(props) {
-	  // this.baseUrl = this.ENV.VUE_APP_BASE_H5
-	  this.baseUrl = 'https://localhost'
+	  this.baseUrl = this.ENV.VUE_APP_BASE_H5
       this.id = props.id;
       uni.showShareMenu();
     },
