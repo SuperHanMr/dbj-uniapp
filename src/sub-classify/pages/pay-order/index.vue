cfb7bb1b9628bf41ccc8475d873af44fb5c88c89
liys <lysd163186@163.com>
2022-03-25 11:02:58 +0800

    商品详情

--- src/sub-classify/pages/pay-order/index.vue
+++ src/sub-classify/pages/pay-order/index.vue
@@ -123,38 +123,6 @@
               </view>
             </view>
           </view>
-<!--          <view
-            class="cost-detail"
-            v-if="(shopItem.deliveryFee || shopItem.totalHandlingFee) && productType === 1"
-          >
-            <view
-              v-if="shopItem.deliveryFee !== undefined"
-              class="price-font"
-            >
-              <text>运费</text>
-              <text>¥{{shopItem.deliveryFee}}</text>
-            </view>
-            <view
-              v-if="shopItem.totalHandlingFee !== undefined"
-              class="price-font"
-            >
-              <text>搬运费</text>
-              <text>¥{{shopItem.totalHandlingFee}}</text>
-            </view>
-          </view> -->
-<!--          <view
-            class="shop-reduce"
-            v-if="shopItem.freeDeliveryCount && productType === 1"
-          >
-            <view class="item-reduce-box">
-              <view class="question-box">本订单已获得了该店铺{{shopItem.freeDeliveryCount}}次免运费权益
-                <text
-                  class="question-icon free-icon"
-                  @click="readExpenses(0)"
-                ></text>
-              </view>
-            </view>
-          </view> -->
         </view>
       </view>
       <view
@@ -259,7 +227,7 @@
         >
         </view>
       </view>
-      <view class="pay-way">
+      <view class="pay-way" :class="{'more_pay':!payChannel}">
         <text>支付方式</text>
 
         <view
@@ -275,8 +243,8 @@
           <text>储值卡支付</text>
 
         </view>
-        <view v-else>
-          <view class="wechat_icon"></view><text>在线支付</text>
+        <view v-else @click="morePayWay">
+          <text>在线支付</text><view class="more_pay_icon"></view>
         </view>
       </view>
       <view class='remarks'>
@@ -473,18 +441,13 @@ export default {
       cardClick: false,
       haveCard: false, //是否有会员卡
       cardBalance: 0, //会员卡余额
-      shareOriginType: "",
+      originType: "",
     };
   },
   computed: {
     payChannel() {
       var res = Number(this.totalPrice) * 100 - this.cardBalance;
       //支付渠道 true 储值卡  false 微信
-      console.log(
-        this.cardClick && res > 0,
-        res,
-        Number(this.totalPrice) * 100
-      );
       if (this.cardClick && res <= 0) {
         return true;
       } else {
@@ -515,7 +478,6 @@ export default {
         }
         return String((res / 100).toFixed(2));
       } else {
-        console.log(this.totalPrice);
         return this.totalPrice;
       }
     },
@@ -531,7 +493,6 @@ export default {
     if (e.from) {
       this.originFrom = e.from;
     }
-    console.log('h5 传递的数据', e);
     if (Number(e.fromPackage) === 1) { // 套包下单
       this.isFromPackage = true;
       this.packageId = e.packageId;
@@ -552,13 +513,11 @@ export default {
     this.unit = e.unit;
     this.level = e.level;
     this.goodDetailId = uni.getStorageSync("goodId");
-    this.shareOriginType = e.shareOriginType;
-    console.log(e.houseId, getApp().globalData.currentHouse.id);
+    this.originType = e.originType;
   },
   onShow() {
     if (uni.getStorageSync("houseListChooseId")) {
       this.houseId = uni.getStorageSync("houseListChooseId");
-      console.log(this.houseId, "this.houseId");
       if (this.$refs.houseDialog) {
         this.$refs.houseDialog.close();
       }
@@ -579,7 +538,6 @@ export default {
     }
 
     this.haveCard = false;
-    console.log("!!!!!!!!!!");
     getBalance().then((e) => {
       if (e != null) {
         this.haveCard = true;
@@ -602,6 +560,9 @@ export default {
       if (this.cardBalance) {
         this.cardClick = !this.cardClick;
       }
+    },
+    morePayWay() {
+      console.log(99999)
     },
     backFrom() {
       uni.navigateBack();
@@ -685,6 +646,7 @@ export default {
               buyCount: this.buyCount,
               unit: this.unit ? this.unit : "",
               level: this.level,
+              origin: this.originType
             },
           ],
           estateId: this.estateId,
@@ -764,7 +726,6 @@ export default {
           data.totalDiscount
         ).toFixed(2);
         var res = Number(this.totalPrice) * 100 - this.cardBalance;
-        console.log(this.totalPrice, res, "res666666666")
         if(res <= 0) {
           this.cardClick = true
         }
@@ -832,6 +793,7 @@ export default {
                 storeId: storeItem.storeId, //店铺id,
                 storeType: 0, //店铺类型 0普通 1设计师",
                 number: skuItem.buyCount, //购买数量",
+                origin: skuItem.origin,
                 params: {}, //与订单无关的参数 如上门时间 doorTime
               };
               this.orderDetails.push({
@@ -894,8 +856,6 @@ export default {
       let orderPrice = Number(
         Number(this.totalPrice).toFixed(2).replace(".", "")
       );
-
-
         //#ifdef MP-WEIXIN
         let params = {
           payType: 1, //"int //支付方式  1微信支付",
@@ -908,7 +868,7 @@ export default {
           orderName: "", //"string //订单名称 可为空",
           details: details,
           isCardPay: this.cardClick,
-          origin: this.shareOriginType,
+          origin: this.originType,
           packageId: this.isFromPackage ? parseInt(this.packageId) : undefined, // 套包下单时需要套包id参数，默认undefined
         };
         this.createOrder(params).then((data) => {
@@ -978,7 +938,7 @@ export default {
           orderName: "", //"string //订单名称 可为空",
           details: details,
           isCardPay: this.cardClick,
-          origin: this.shareOriginType,
+          origin: this.originType,
           packageId: this.isFromPackage ? parseInt(this.packageId) : undefined, // 套包下单时需要套包id参数，默认undefined
         };
         this.createOrder(params).then((data) => {
@@ -1373,13 +1333,15 @@ export default {
     overflow: hidden;
     border-radius: 32rpx;
   }
-
-  .pay-way .wechat_icon {
-    vertical-align: sub;
+  .more_pay{
+    padding-right: 10rpx;
+  }
+  .pay-way .more_pay_icon {
+    vertical-align: middle;
     display: inline-block;
-    width: 32rpx;
-    height: 32rpx;
-    // background-image: url("../../static/image/wechat_icon.png");
+    width: 48rpx;
+    height: 52rpx;
+    background-image: url("../../static/image/more_pay_icon.png");
     background-size: contain;
     margin-right: 12rpx;
   }
