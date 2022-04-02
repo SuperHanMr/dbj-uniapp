afa2f050c480f6a75b4fc1123271df0ce042d6a1
liys <lysd163186@163.com>
2022-04-02 10:25:40 +0800

    商城

--- src/components/classify-shop/shop-list-item.vue
+++ src/components/classify-shop/shop-list-item.vue
@@ -2,7 +2,7 @@
 	<view class="item" @click="toGoodsDetail(item.product.skuId)">
 		<image class="img" :src="item.product.spuImage | imgFormat" mode="aspectFill"></image>
 		<view class="info">
-			<view class="category">
+			<view class="category" v-if="categoryListHandler(item.product.categories) && categoryListHandler(item.product.categories).length > 0">
 				<view class="category-item-box">
 					<text class="category-item"
 						v-for="(category, categoryIndex) in categoryListHandler(item.product.categories)"
