5f82701099e109c4ffdbc2d368f9749df42a8512
liys <lysd163186@163.com>
2022-04-17 19:59:11 +0800

    商品

--- src/sub-classify/pages/product-detail/service-product-index.vue
+++ src/sub-classify/pages/product-detail/service-product-index.vue
@@ -1,22 +1,28 @@
 <template>
-  <view>
-    
-  </view>
+	<view>
+		<web-view :src="`${baseUrl}/app-pages/product-detail/service-product.html?spuId=${spuId}&houseId=${houseId}&source=small&token=${token}`">
+		</web-view>
+	</view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      
-    }
-  }
+	export default {
+		data() {
+			return {
+				baseUrl: '',
+				spuId:'',
+        houseId: 0,
+        token: ''
+			}
+		},
+		onLoad(e) {
+      this.baseUrl = this.ENV.VUE_APP_BASE_H5
+      thos.token = getApp().globalData.token
+      this.spuId = e.spuId
+      this.houseId = e.houseId
+		}
+	}
 </script>
 
 <style>
-
 </style>
