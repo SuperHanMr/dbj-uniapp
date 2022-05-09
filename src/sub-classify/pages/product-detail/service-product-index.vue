d27627626213edfbc3d0af1b34e893ea8d466f11
HanYongHui <xd66620147@163.com>
2022-05-09 12:32:07 +0800

    dev-2.0.15

--- src/sub-classify/pages/product-detail/service-product-index.vue
+++ src/sub-classify/pages/product-detail/service-product-index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
-		<web-view :src="`${baseUrl}/app-pages/product-detail/service-product.html?spuId=${spuId}&houseId=${houseId}&from=${from}&productId=${productId}&time=${time}&source=small&sourceIMId=${sourceIMId}&token=${token}`">
+		<web-view
+			:src="`${baseUrl}/app-pages/product-detail/service-product.html?spuId=${spuId}&houseId=${houseId}&from=${from}&productId=${productId}&time=${time}&source=small&sourceIMId=${sourceIMId}&token=${token}`">
 		</web-view>
 	</view>
 </template>
@@ -9,24 +10,24 @@
 	export default {
 		data() {
 			return {
-        time: 0,
-        from: '',
+				time: 0,
+				from: '',
 				baseUrl: '',
-        sourceIMId: getApp().globalData.userInfo.id,
+				sourceIMId: getApp().globalData.userInfo.id,
 				spuId: 0,
-        productId: 0,
-        houseId: 0,
-        token: ""
+				productId: 0,
+				houseId: 0,
+				token: ""
 			}
 		},
 		onLoad(e) {
-      this.baseUrl = this.ENV.VUE_APP_BASE_H5
-      this.token = getApp().globalData.token
-      this.spuId = e.spuId
-      this.houseId = e.houseId ? e.houseId : getApp().globalData.currentHouse.id;
-      this.from = e.from
-      this.productId = e.productId
-      this.time = e.time
+			this.baseUrl = this.ENV.VUE_APP_BASE_H5
+			this.token = getApp().globalData.token
+			this.spuId = e.spuId
+			this.houseId = e.houseId ? e.houseId : getApp().globalData.currentHouse.id;
+			this.from = e.from
+			this.productId = e.productId
+			this.time = e.time
 		}
 	}
 </script>
