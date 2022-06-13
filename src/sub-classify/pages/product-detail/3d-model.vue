979b8e2a1bac623a3d5cd891f1007a697a797326
HanYongHui <xd66620147@163.com>
2022-06-13 20:03:13 +0800

    小程序 3d模型页面开发

---
+++ src/sub-classify/pages/product-detail/3d-model.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 3d模型展示页面
+ * @Author: HanYongHui
+ * @Date: 2022-06-09 15:08:30
+ * @LastEditTime: 2022-06-09 17:16:27
+ * @LastEditors: HanYongHui
+-->
+<!--  -->
+<template>
+  <web-view :src="`${baseUrl}/app-pages/3d-model/index.html?id=${skuId}`" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      baseUrl: "",
+      skuId: "",
+    };
+  },
+
+  onLoad(options) {
+    // this.baseUrl = this.ENV.VUE_APP_BASE_H5;
+    this.baseUrl = "https://localhost";
+    this.skuId = options.skuId;
+  },
+};
+</script>
+<style lang='scss' scoped>
+</style>
