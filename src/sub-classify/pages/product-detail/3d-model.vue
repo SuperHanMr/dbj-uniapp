018947d4f06397889dede611b2155b8d40c9a342
HanYongHui <xd66620147@163.com>
2022-06-20 16:50:30 +0800

    小程序 3d模型页面开发

--- src/sub-classify/pages/product-detail/3d-model.vue
+++ src/sub-classify/pages/product-detail/3d-model.vue
@@ -2,7 +2,7 @@
  * @Description: 3d模型展示页面
  * @Author: HanYongHui
  * @Date: 2022-06-09 15:08:30
- * @LastEditTime: 2022-06-09 17:16:27
+ * @LastEditTime: 2022-06-14 10:59:47
  * @LastEditors: HanYongHui
 -->
 <!--  -->
@@ -20,8 +20,8 @@ export default {
   },
 
   onLoad(options) {
-    // this.baseUrl = this.ENV.VUE_APP_BASE_H5;
-    this.baseUrl = "https://localhost";
+    this.baseUrl = this.ENV.VUE_APP_BASE_H5;
+    // this.baseUrl = "https://localhost";
     this.skuId = options.skuId;
   },
 };
